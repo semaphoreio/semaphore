a32b3e3a3724123447062269c14c0ed4c33a92cb
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-02-24 16:12:41 -0300

    fix: notificiation elixir-utils

--- notifications/mix.exs
+++ notifications/mix.exs
@@ -39,7 +39,7 @@ defmodule Notifications.Mixfile do
       {:grpc_mock, github: "renderedtext/grpc-mock", only: [:dev, :test]},
       {:paginator, "~> 1.0"},
       {:tackle, github: "renderedtext/ex-tackle"},
-      {:util, github: "renderedtext/elixir-util", branch: "rw/string_enums"},
+      {:util, github: "renderedtext/elixir-util"},
       {:sentry, "~> 8.0"},
       {:hackney, "~> 1.8"},
       {:sentry_grpc, github: "renderedtext/sentry_grpc"},
