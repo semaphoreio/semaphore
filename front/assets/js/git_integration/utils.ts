3b9de20b8e4e1a06922a03e1e0f5ec5c743c9ef5
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-02-25 16:51:39 -0300

    fix: indent

--- front/assets/js/git_integration/utils.ts
+++ front/assets/js/git_integration/utils.ts
@@ -1,15 +1,15 @@
 export const createOrderMap = <T extends PropertyKey>(orderArray: T[]): Record<T, number> =>
-    orderArray.reduce((map, type, index) => {
-        map[type] = index;
-        return map;
-    }, {} as Record<T, number>);
+  orderArray.reduce((map, type, index) => {
+    map[type] = index;
+    return map;
+  }, {} as Record<T, number>);
 
 export const sortByOrder = <T extends PropertyKey>(items: T[], orderMap: Record<T, number>) =>
-    [...items].sort((a, b) => (orderMap[a] ?? Infinity) - (orderMap[b] ?? Infinity));
+  [...items].sort((a, b) => (orderMap[a] ?? Infinity) - (orderMap[b] ?? Infinity));
 
 export const sortObjectByOrder = <T extends Record<string, any>>(
-    items: T[],
-    orderMap: Record<PropertyKey, number>,
-    key: keyof T
+  items: T[],
+  orderMap: Record<PropertyKey, number>,
+  key: keyof T
 ) =>
-    [...items].sort((a, b) => (orderMap[a[key]] ?? Infinity) - (orderMap[b[key]] ?? Infinity));
+  [...items].sort((a, b) => (orderMap[a[key]] ?? Infinity) - (orderMap[b[key]] ?? Infinity));
