0b2de91bfb6874f6b0eae3f455d95b90c087b622
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-02-25 16:29:53 -0300

    fix: ts types

--- front/assets/js/git_integration/utils.ts
+++ front/assets/js/git_integration/utils.ts
@@ -1,11 +1,15 @@
-export const createOrderMap = (orderArray: any[]): Record<any, number> =>
-  orderArray.reduce((map, type, index) => {
-    map[type] = index;
-    return map;
-  }, {});
+export const createOrderMap = <T extends PropertyKey>(orderArray: T[]): Record<T, number> =>
+    orderArray.reduce((map, type, index) => {
+        map[type] = index;
+        return map;
+    }, {} as Record<T, number>);
 
-export const sortByOrder = <T>(items: T[], orderMap: Record<any, number>) =>
-  [...items].sort((a, b) => (orderMap[a] ?? Infinity) - (orderMap[b] ?? Infinity));
+export const sortByOrder = <T extends PropertyKey>(items: T[], orderMap: Record<T, number>) =>
+    [...items].sort((a, b) => (orderMap[a] ?? Infinity) - (orderMap[b] ?? Infinity));
 
-export const sortObjectByOrder = <T>(items: T[], orderMap: Record<any, number>, key: string) =>
-  [...items].sort((a, b) => (orderMap[a[key]] ?? Infinity) - (orderMap[b[key]] ?? Infinity));
+export const sortObjectByOrder = <T extends Record<string, any>>(
+    items: T[],
+    orderMap: Record<PropertyKey, number>,
+    key: keyof T
+) =>
+    [...items].sort((a, b) => (orderMap[a[key]] ?? Infinity) - (orderMap[b[key]] ?? Infinity));
