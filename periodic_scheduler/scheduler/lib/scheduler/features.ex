b367abc0c31a2b8cd725890a257f58b904da5f23
Lucas Pinheiro <lpinheiro@renderedtext.com>
2025-02-26 10:33:13 -0300

    fix: update periodic-scheduler and secrethub to use cache from library (#57)
    Follow-up to https://github.com/semaphoreio/semaphore/pull/15. We don't
    need to cache on the application now, since the library already has
    logic for this.

--- periodic_scheduler/scheduler/lib/scheduler/features.ex
+++ periodic_scheduler/scheduler/lib/scheduler/features.ex
@@ -3,58 +3,15 @@ defmodule Scheduler.FeatureHubProvider do
   use FeatureProvider.Provider
   require Logger
 
-  @cache_key Elixir.Scheduler.FeatureHubProvider
-
   alias InternalApi.Feature.{
     OrganizationFeature,
     Availability
   }
 
-  @namespace "scheduler.feature_provider"
-  @cache_version :crypto.hash(:md5, File.read(__ENV__.file) |> elem(1)) |> Base.encode64()
-
   defp url(), do: Application.get_env(:scheduler, :feature_api_grpc_endpoint)
-
-  defp cache_key(org_id, operation),
-    do: "#{@namespace}/#{@cache_version}/#{org_id}/#{operation}"
 
   @impl FeatureProvider.Provider
   def provide_features(org_id, opts) do
-    ttl = Keyword.get(opts, :ttl, :timer.minutes(15))
-    cache_key = cache_key(org_id, "list_organization_features")
-
-    Cachex.fetch(@cache_key, cache_key, fn ->
-      do_list_features(org_id)
-      |> case do
-        {:ok, features} ->
-          {:commit, features}
-
-        _err ->
-          {:ignore, []}
-      end
-    end)
-    |> case do
-      {:commit, features} ->
-        Cachex.expire(@cache_key, cache_key, ttl)
-        {:ok, features}
-
-      {:ok, features} ->
-        {:ok, features}
-
-      {:ignore, _} ->
-        {:ok, []}
-
-      _ ->
-        {:ok, []}
-    end
-  end
-
-  @impl FeatureProvider.Provider
-  def provide_machines(_org_id, _) do
-    {:ok, []}
-  end
-
-  defp do_list_features(org_id) do
     alias InternalApi.Feature.ListOrganizationFeaturesRequest, as: Request
     alias InternalApi.Feature.FeatureService.Stub
 
@@ -87,6 +44,11 @@ defmodule Scheduler.FeatureHubProvider do
           {:ok, []}
       end
     end)
+  end
+
+  @impl FeatureProvider.Provider
+  def provide_machines(_org_id, _) do
+    {:ok, []}
   end
 
   defp feature_from_grpc(%OrganizationFeature{feature: feature, availability: availability}) do
