2ea5e730d4f852ad56cc5868fdb240c118c41bc0
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-02-26 12:49:36 +0100

    Fix default user avatar

--- front/lib/front/activity_monitor.ex
+++ front/lib/front/activity_monitor.ex
@@ -403,8 +403,11 @@ defmodule Front.ActivityMonitor do
     )
   end
 
-  defp get_user_avatar(nil), do: FrontWeb.SharedHelpers.assets_path()
-  defp get_user_avatar(user), do: user.avatar_url || FrontWeb.SharedHelpers.assets_path()
+  defp get_user_avatar(nil), do: default_user_avatar()
+  defp get_user_avatar(user), do: user.avatar_url || default_user_avatar()
+
+  defp default_user_avatar,
+    do: "#{FrontWeb.SharedHelpers.asset_path()}/images/semaphore-logo-sign-black.svg"
 
   defp get_user_name(nil), do: Application.get_env(:front, :default_user_name)
   defp get_user_name(user), do: user.name || Application.get_env(:front, :default_user_name)
