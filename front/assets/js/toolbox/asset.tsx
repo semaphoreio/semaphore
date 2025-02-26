bef3d4f599df1115e69b06239e9cc3b03e8a61fb
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-02-26 11:03:55 +0100

    Fix url construction within preact

--- front/assets/js/toolbox/asset.tsx
+++ front/assets/js/toolbox/asset.tsx
@@ -9,6 +9,6 @@ export default function ({
   path: path,
   ...props
 }: AssetProps) {
-  const url = new URL(`./${path}`, `${assetPath}/`).href;
+  const url = new URL(`${assetPath}/${path}`, `${location.origin}/`).href;
   return <img src={url} {...props}/>;
 }
