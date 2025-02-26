55a8885af0136ed1ce3a228b70b991906f8ba6a2
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-02-26 01:05:04 +0100

    Update build.js

--- front/assets/build.js
+++ front/assets/build.js
@@ -1,4 +1,6 @@
 const esbuild = require('esbuild')
+const fs = require('fs')
+const path = require('path')
 
 const bundle = true
 const logLevel = process.env.ESBUILD_LOG_LEVEL || 'silent'
@@ -7,6 +9,29 @@ const watch = !!process.env.ESBUILD_WATCH
 const plugins = [
   // Add and configure plugins here
 ]
+
+const outputDir = '../priv/static/assets'
+
+const copyDir = (src, dest) => {
+  const files = fs.readdirSync(src)
+  
+  for (const file of files) {
+    const srcPath = path.join(src, file)
+    const destPath = path.join(dest, file)
+    fs.copyFileSync(srcPath, destPath)
+  }
+}
+
+// Function to copy static assets
+const copyAssets = () => {
+  console.log('Copying original assets to output directory...')
+  
+  copyDir('css', path.join(outputDir, 'css'), { overwrite: true })
+  copyDir('fonts', path.join(outputDir, 'fonts'), { overwrite: true })
+  copyDir('images', path.join(outputDir, 'images'), { overwrite: true })
+  
+  console.log('Assets copied successfully')
+}
 
 const buildOptions = {
   minify: true,
@@ -35,6 +60,7 @@ const buildOptions = {
 if (watch) {
   esbuild.context(buildOptions).then(context => {
     context.watch()
+    copyAssets()
     process.stdin.on('close', () => {
       context.dispose()
       process.exit(0)
@@ -42,5 +68,10 @@ if (watch) {
     process.stdin.resume()
   })
 } else {
-  esbuild.build(buildOptions)
+  esbuild.build(buildOptions).then(() => {
+    copyAssets()
+  }).catch(error => {
+    console.error('Build error:', error)
+    process.exit(1)
+  })
 }
