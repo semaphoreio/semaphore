e9c4668afa94be80a60baa0ea465c1a583034ad8
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-03-07 16:03:57 -0300

    fix tests

--- guard/test/guard/grpc_servers/user_server_test.exs
+++ guard/test/guard/grpc_servers/user_server_test.exs
@@ -967,7 +967,7 @@ defmodule Guard.GrpcServers.UserServerTest do
           {:ok,
            %Tesla.Env{status: 200, body: %{"access_token" => "mock_token", "expires_in" => 3600}}}
 
-        %{method: :post, url: "https://api.github.com/applications/github_client_id/token"} ->
+        %{method: :get, url: "https://api.github.com"} ->
           {:ok, %Tesla.Env{status: 200, body: %{}}}
       end)
 
@@ -1094,9 +1094,8 @@ defmodule Guard.GrpcServers.UserServerTest do
          } do
       Tesla.Mock.mock_global(fn
         %{
-          method: :post,
-          url: "https://api.github.com/applications/github_client_id/token",
-          body: "{\"access_token\":\"token\"}"
+          method: :get,
+          url: "https://api.github.com"
         } ->
           json(%{"valid" => "valid"})
       end)
