2e4f715d3135da661b3884026d5a6b00621f2e79
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-03-10 03:19:46 +0100

    Create saml jit user

--- ee/rbac/lib/rbac/repo/saml_jit_user.ex
+++ ee/rbac/lib/rbac/repo/saml_jit_user.ex
@@ -1,6 +1,7 @@
 defmodule Rbac.Repo.SamlJitUser do
   use Rbac.Repo.Schema
   alias Rbac.Repo
+  import Ecto.Query, only: [where: 3]
 
   @timestamps_opts [type: :utc_datetime]
 
@@ -32,25 +33,15 @@ defmodule Rbac.Repo.SamlJitUser do
     timestamps()
   end
 
-  # def new(integration, payload) do
-  #   %__MODULE__{
-  #     integration_id: integration.id,
-  #     org_id: integration.org_id,
-  #     attributes: payload,
-  #     email: email_from_payload(payload),
-  #     state: :pending
-  #   }
-  # end
-
-  def changeset(user, params \\ %{}) do
-    user
-    |> cast(params, @updatable_fields)
-    |> validate_required(@required_fields)
+  def create(integration, email, attributes) do
+    new(integration, email, attributes)
+    |> changeset()
+    |> Rbac.Repo.insert()
   end
 
   def find_by_email(integration, email) do
     __MODULE__
-    |> where([u], u.integration_id == ^integration.id and e.email == ^email)
+    |> where([u], u.integration_id == ^integration.id and u.email == ^email)
     |> Repo.one()
     |> case do
       nil -> {:error, :not_found}
@@ -60,5 +51,21 @@ defmodule Rbac.Repo.SamlJitUser do
 
   def connect_user(saml_jit_user, user_id) do
     changeset(saml_jit_user, %{user_id: user_id}) |> Repo.update()
+  end
+
+  defp new(integration, email, attributes) do
+    %__MODULE__{
+      integration_id: integration.id,
+      org_id: integration.org_id,
+      attributes: attributes,
+      email: email,
+      state: :pending
+    }
+  end
+
+  defp changeset(user, params \\ %{}) do
+    user
+    |> cast(params, @updatable_fields)
+    |> validate_required(@required_fields)
   end
 end
