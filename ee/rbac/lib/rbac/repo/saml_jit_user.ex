b3dcaa104f149ee19a363a03d0f5971d4c4739db
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-03-10 01:54:52 +0100

    Create saml jit users table

---
+++ ee/rbac/lib/rbac/repo/saml_jit_user.ex
@@ -0,0 +1,43 @@
+defmodule Rbac.Repo.SamlJitUser do
+  use Rbac.Repo.Schema
+
+  @timestamps_opts [type: :utc_datetime]
+
+  @required_fields [
+    :org_id,
+    :integration_id,
+    :attributes,
+    :state,
+    :email
+  ]
+
+  @updatable_fields [
+    :attributes,
+    :state,
+    :email,
+    :updated_at,
+    :user_id
+  ]
+
+  schema "saml_jit_users" do
+    belongs_to(:integration, Rbac.Repo.OktaIntegration)
+
+    field(:org_id, :binary_id)
+    field(:attributes, :map)
+    field(:state, Ecto.Enum, values: [:pending, :processed])
+    field(:user_id, :binary_id)
+    field(:email, :string)
+
+    timestamps()
+  end
+
+  # def new(integration, payload) do
+  #   %__MODULE__{
+  #     integration_id: integration.id,
+  #     org_id: integration.org_id,
+  #     attributes: payload,
+  #     email: email_from_payload(payload),
+  #     state: :pending
+  #   }
+  # end
+end
