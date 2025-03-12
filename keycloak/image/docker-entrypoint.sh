263e45bf52ed4d67e74eb5dd539cf2bb87555ae4
Radosław Woźniak <184065+radwo@users.noreply.github.com>
2025-03-12 22:34:05 +0100

    set ingress to nginx

--- keycloak/image/docker-entrypoint.sh
+++ keycloak/image/docker-entrypoint.sh
@@ -10,6 +10,7 @@ if [ -z $KC_DB_USERNAME ];      then echo "DB username not set" && exit 1; fi
 if [ -z $KC_DB_URL_DATABASE ];  then echo "DB name not set"     && exit 1; fi
 
 echo "Creating keycloak database..."
+PGPASSWORD=${KC_DB_PASSWORD} dropdb -h ${KC_DB_URL_HOST} -p ${KC_DB_URL_PORT} -U ${KC_DB_USERNAME} ${KC_DB_URL_DATABASE} || true
 PGPASSWORD=${KC_DB_PASSWORD} createdb -h ${KC_DB_URL_HOST} -p ${KC_DB_URL_PORT} -U ${KC_DB_USERNAME} ${KC_DB_URL_DATABASE} -E UTF8 || true
 
 echo "Starting keycloak..."
