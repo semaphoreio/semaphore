ba84081487e2eca951da9281608ccae9a052b478
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-03-19 12:43:45 +0100

    Assign group membership for provisioned saml users

--- ee/rbac/test/rbac/okta/saml/provisioner/add_user_test.exs
+++ ee/rbac/test/rbac/okta/saml/provisioner/add_user_test.exs
@@ -2,6 +2,7 @@ defmodule Rbac.Okta.Saml.Provisioner.AddUser.Test do
   use Rbac.RepoCase, async: true
   alias Rbac.Okta.Saml.JitProvisioner.AddUser
   alias Rbac.{Repo, FrontRepo}
+  alias Support.Factories
 
   @org_id Ecto.UUID.generate()
 
@@ -23,7 +24,7 @@ defmodule Rbac.Okta.Saml.Provisioner.AddUser.Test do
     test "When the org has custom mapping but w/o any role mappings", ctx do
       {:ok, role} = Repo.RbacRole.get_role_by_name("Admin", "org_scope", ctx.jit_user.org_id)
 
-      Support.Factories.IdpGroupMapping.insert(
+      Factories.IdpGroupMapping.insert(
         organization_id: ctx.jit_user.org_id,
         default_role_id: role.id
       )
@@ -36,7 +37,7 @@ defmodule Rbac.Okta.Saml.Provisioner.AddUser.Test do
 
     test "When the org has custom role mappings" do
       {:ok, jit_user} =
-        Support.Factories.SamlJitUser.insert(
+        Factories.SamlJitUser.insert(
           org_id: @org_id,
           attributes: %{"role" => ["role_1", "test_role"]}
         )
@@ -44,7 +45,7 @@ defmodule Rbac.Okta.Saml.Provisioner.AddUser.Test do
       {:ok, admin} = Repo.RbacRole.get_role_by_name("Admin", "org_scope", jit_user.org_id)
       {:ok, owner} = Repo.RbacRole.get_role_by_name("Owner", "org_scope", jit_user.org_id)
 
-      Support.Factories.IdpGroupMapping.insert(
+      Factories.IdpGroupMapping.insert(
         organization_id: jit_user.org_id,
         default_role_id: admin.id,
         role_mapping: [%{idp_role_id: "test_role", semaphore_role_id: owner.id}]
@@ -54,6 +55,36 @@ defmodule Rbac.Okta.Saml.Provisioner.AddUser.Test do
 
       assert_user_created(jit_user)
       assert_role_assigned(jit_user, "Owner")
+    end
+
+    test "When the org has custom group mappings" do
+      {:ok, jit_user} =
+        Factories.SamlJitUser.insert(
+          org_id: @org_id,
+          attributes: %{"memberOf" => ["g1", "g2", "unmapped_group"]}
+        )
+
+      {:ok, group1} = Factories.Group.insert(organization_id: @org_id)
+      {:ok, group2} = Factories.Group.insert(organization_id: @org_id)
+      {:ok, member} = Repo.RbacRole.get_role_by_name("Member", "org_scope", jit_user.org_id)
+
+      Factories.IdpGroupMapping.insert(
+        organization_id: jit_user.org_id,
+        default_role_id: member.id,
+        group_mapping: [
+          %{idp_group_id: "g1", semaphore_group_id: group1.id},
+          %{idp_group_id: "g2", semaphore_group_id: group2.id}
+        ]
+      )
+
+      {:ok, jit_user} = AddUser.run(jit_user)
+
+      assert_user_created(jit_user)
+      assert_role_assigned(jit_user, "Member")
+      assert_group_request(jit_user.user_id, group1.id)
+      assert_group_request(jit_user.user_id, group2.id)
+
+      assert Repo.GroupManagementRequest |> Repo.aggregate(:count) == 2
     end
   end
 
@@ -76,6 +107,14 @@ defmodule Rbac.Okta.Saml.Provisioner.AddUser.Test do
                s.role_id == ^role.id and
                is_nil(s.project_id)
            )
+           |> Repo.exists?()
+  end
+
+  defp assert_group_request(user_id, group_id) do
+    import Ecto.Query
+
+    assert Repo.GroupManagementRequest
+           |> where([r], r.user_id == ^user_id and r.group_id == ^group_id and r.action == :add)
            |> Repo.exists?()
   end
 end
