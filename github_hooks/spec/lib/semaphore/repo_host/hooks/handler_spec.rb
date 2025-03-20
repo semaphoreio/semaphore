a99b89608866edf81f92ab1f746d7145e51e1c48
Lucas Pinheiro <lpinheiro@renderedtext.com>
2025-03-20 17:58:01 -0300

    use block on assertion

--- github_hooks/spec/lib/semaphore/repo_host/hooks/handler_spec.rb
+++ github_hooks/spec/lib/semaphore/repo_host/hooks/handler_spec.rb
@@ -319,7 +319,9 @@ RSpec.describe Semaphore::RepoHost::Hooks::Handler do
           it "logs the error and raises again" do
             expect(@logger).to receive(:error).with("Unknown error", error: "Oops")
 
-            expect(described_class.run(@workflow, @logger)).to raise_error(RepoHost::RemoteException::Unknown)
+            expect do
+              described_class.run(@workflow, @logger)
+            end.to raise_error(RepoHost::RemoteException::Unknown)
           end
         end
 
