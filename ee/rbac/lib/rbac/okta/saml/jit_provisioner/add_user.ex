a1256fbbe467481837732a035bffda0a6417d170
Veljko Maksimovic <45179708+VeljkoMaksimovic@users.noreply.github.com>
2025-03-20 12:39:48 +0100

    Update ee/rbac/lib/rbac/okta/saml/jit_provisioner/add_user.ex
    Co-authored-by: Pedro Leão <60622592+forestileao@users.noreply.github.com>

--- ee/rbac/lib/rbac/okta/saml/jit_provisioner/add_user.ex
+++ ee/rbac/lib/rbac/okta/saml/jit_provisioner/add_user.ex
@@ -116,7 +116,7 @@ defmodule Rbac.Okta.Saml.JitProvisioner.AddUser do
       :ok
     else
       {:ok, rbi} = RoleBindingIdentification.new(user_id: user_id, org_id: org_id)
-      # Although this role is assigned through :saml_jit, it can be updated and
+      # Although this role is assigned through saml_jit, it can be updated and
       # removed manually, hence it is marked as "manually_assigned"
       {:ok, nil} = RoleManagement.assign_role(rbi, role_id, :manually_assigned)
 
