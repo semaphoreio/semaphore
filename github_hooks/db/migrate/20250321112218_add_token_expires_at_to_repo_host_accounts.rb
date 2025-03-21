a7e69a9064f819fbf8f0827af70a7a51b6e2be38
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-03-21 02:30:13 -0300

    fix: ruby token_expires_at db type

--- github_hooks/db/migrate/20250321112218_add_token_expires_at_to_repo_host_accounts.rb
+++ github_hooks/db/migrate/20250321112218_add_token_expires_at_to_repo_host_accounts.rb
@@ -1,5 +1,5 @@
 class AddTokenExpiresAtToRepoHostAccounts < ActiveRecord::Migration[5.1]
   def change
-    add_column :repo_host_accounts, :token_expires_at, :utc_datetime, null: true, default: nil
+    add_column :repo_host_accounts, :token_expires_at, :datetime, null: true, default: nil
   end
 end
