ca6301f12f45517f9c8efe08f130fa5d5381c8db
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-03-21 15:51:24 +0100

    When deleting user, also disconnect okta and saml users

--- ee/rbac/lib/rbac/repo/saml_jit_user.ex
+++ ee/rbac/lib/rbac/repo/saml_jit_user.ex
@@ -49,8 +49,18 @@ defmodule Rbac.Repo.SamlJitUser do
     end
   end
 
-  def connect_user(%__MODULE__{} = user, user_id) do
-    changeset(user, %{user_id: user_id}) |> Repo.update()
+  def find_by_user_id(user_id) do
+    __MODULE__
+    |> where([u], u.user_id == ^user_id)
+    |> Repo.all()
+  end
+
+  def connect_user(%__MODULE__{} = saml_jit_user, user_id) do
+    changeset(saml_jit_user, %{user_id: user_id}) |> Repo.update()
+  end
+
+  def disconnect_user(%__MODULE__{} = saml_jit_user) do
+    changeset(saml_jit_user, %{user_id: nil}) |> Repo.update()
   end
 
   def construct_name(%__MODULE__{} = user) do
