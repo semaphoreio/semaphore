4f5298324c59eb6ed24f46e0dd04294fcfa42d12
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-04-01 15:18:53 +0200

    Add cluster_name output for eks terraform

--- ephemeral_environment/terraform/eks/outputs.tf
+++ ephemeral_environment/terraform/eks/outputs.tf
@@ -2,3 +2,9 @@ output "ssl_cert_name" {
   description = "ARN of the SSL certificate"
   value       = aws_acm_certificate.cert.arn
 }
+
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = aws_eks_cluster.cluster.name
+}
+  
