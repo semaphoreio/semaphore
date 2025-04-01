7c00feb7cb07689e211d427b897db1fb08af57c1
Pedro Leão <60622592+forestileao@users.noreply.github.com>
2025-04-01 11:29:25 -0300

    fix(guard,rbac): Update same_site cookie policy to Lax (#193)

--- guard/test/guard/id/api_test.exs
+++ guard/test/guard/id/api_test.exs
@@ -476,7 +476,7 @@ defmodule Guard.Id.Api.Test do
       {_, cookie} = Enum.find(response.headers, fn h -> elem(h, 0) == "set-cookie" end)
 
       assert cookie =~ "semaphore_auth_state="
-      assert cookie =~ "secure; HttpOnly; SameSite=Strict"
+      assert cookie =~ "secure; HttpOnly; SameSite=Lax"
 
       assert response.body =~ "/protocol/openid-connect/auth"
       assert response.body =~ "localhost"
