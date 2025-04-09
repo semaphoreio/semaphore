4416d443f4e5b8df02d053f12bb1a3584ec1023f
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-04-08 23:56:06 -0300

    feat: add deleted by for future hard deletion

--- projecthub/priv/repo/migrations/20250407200300_add_deleted_at_for_projects.exs
+++ projecthub/priv/repo/migrations/20250407200300_add_deleted_at_for_projects.exs
@@ -4,6 +4,7 @@ defmodule Projecthub.Repo.Migrations.AddDeletedAtForProjects do
   def change do
     alter table("projects") do
       add :deleted_at, :utc_datetime, null: true, default: nil
+      add :deleted_by, :binary_id, null: true, default: nil
     end
   end
 end
