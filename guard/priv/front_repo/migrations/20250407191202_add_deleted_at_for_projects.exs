930e4cd5c9f41e0550a7187375625e5ea102c374
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-04-09 02:03:02 -0300

    delete unused migration

--- guard/priv/front_repo/migrations/20250407191202_add_deleted_at_for_projects.exs
+++
@@ -1,9 +0,0 @@
-defmodule Guard.FrontRepo.Migrations.AddDeletedAtForProjects do
-  use Ecto.Migration
-
-  def change do
-    alter table("projects") do
-      add :deleted_at, :utc_datetime, null: true, default: nil
-    end
-  end
-end
