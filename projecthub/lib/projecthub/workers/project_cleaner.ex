80aa6bbb99e90ca48b3674221b7b21cbe4458251
Pedro Foresti Leão <pforesti@renderedtext.com>
2025-04-11 09:45:04 -0300

    update to simple enum.each

--- projecthub/lib/projecthub/workers/project_cleaner.ex
+++ projecthub/lib/projecthub/workers/project_cleaner.ex
@@ -5,24 +5,12 @@ defmodule Projecthub.Workers.ProjectCleaner do
 
   require Logger
 
-  @chunk_size 10
-
   def process do
     Project.find_candidates_for_hard_destroy()
-    |> Stream.chunk_every(@chunk_size)
-    |> Stream.each(fn projects ->
-      projects
-      |> Task.async_stream(
-        fn project ->
-          Logger.info("Hard destroying project #{project.id}")
-          Project.hard_destroy(project, project.deleted_by)
-        end,
-        max_concurrency: @chunk_size,
-        timeout: :infinity
-      )
-      |> Stream.run()
+    |> Enum.each(fn project ->
+      Logger.info("Hard destroying project #{project.id}")
+      Project.hard_destroy(project, project.deleted_by)
     end)
-    |> Stream.run()
 
     :ok
   end
