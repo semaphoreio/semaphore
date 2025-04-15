688f7c3a0ee4375ee1adcc6270acf049dca0f0ec
Amir Hasanbasic <ahasanbasic@renderedtext.com>
2025-04-15 08:39:45 +0200

    fix(e2e): try requiring Wallaby DSL

--- e2e/test/support/ui_test_case.ex
+++ e2e/test/support/ui_test_case.ex
@@ -14,6 +14,7 @@ defmodule E2E.UI.UserTestCase do
     quote do
       use ExUnit.Case, async: false
       use Wallaby.DSL
+      require Wallaby.Browser
 
       @moduletag :user
       @moduletag :browser
