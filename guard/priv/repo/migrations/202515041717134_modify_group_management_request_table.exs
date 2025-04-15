fde05711fb344bd055c5a12e9985b2a5c67cce36
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-04-15 17:37:09 +0200

    Add requester id to group management requests

---
+++ guard/priv/repo/migrations/202515041717134_modify_group_management_request_table.exs
@@ -0,0 +1,10 @@
+defmodule Guard.Repo.Migrations.ModifyGroupManagementRequestTable do
+  use Ecto.Migration
+
+  def change do
+    alter table(:group_management_request) do
+      modify :user_id, :binary_id, null: true
+      add :requester_id, :binary_id, null: true
+    end
+  end
+end
