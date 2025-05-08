23b7b8e7e55ab3449c2da84ae2c4f536b98dbff6
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-05-08 19:28:04 +0200

    Fix popstate

--- front/assets/js/workflow_list.js
+++ front/assets/js/workflow_list.js
@@ -1,5 +1,3 @@
-import $ from "jquery";
-
 import { TokenPagination } from "./pollman_list/token_pagination"
 import { PollmanList } from "./pollman_list/list"
 
@@ -12,8 +10,10 @@ export var WorkflowList = {
     this.pagination = new TokenPagination("#workflow-lists")
 
     let pollmanList = new PollmanList;
+    const container = document.querySelector("#workflow-lists"); // Get the actual DOM element instead of just the selector string
 
     let updatePollman = function(container, params) {
+      console.log("EXECUTING UPDATE POLLMAN", container, params)
       pollmanList.updateOptionsAndFetch(container, params);
 
       const currentUrl = new URL(window.location.href);
@@ -21,6 +21,23 @@ export var WorkflowList = {
       currentUrl.searchParams.set('direction', params.direction);
       window.history.pushState({}, '', currentUrl.toString());
     }
+
+    window.addEventListener('popstate', function() {
+      console.log("EXECUTING POPSTATE")
+      const currentUrl = new URL(window.location.href);
+      const pageToken = currentUrl.searchParams.get('page_token');
+      const direction = currentUrl.searchParams.get('direction');
+      console.log(container)
+      console.log(currentUrl)
+      console.log(pageToken)
+      console.log(direction)
+      console.log(currentUrl.searchParams)
+
+      pollmanList.updateOptionsAndFetch(container, {
+        page_token: pageToken,
+        direction: direction
+      });
+    });
 
     this.pagination.onUpdate(updatePollman);
   }
