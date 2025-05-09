116321bea2fab10558c1a6a8b7477c4a77648a0b
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2025-05-09 13:05:37 +0200

    fix(repository_hub): properly construct webhook for generic git repositories

--- repository_hub/lib/repository_hub/adapters/git/check_webhook_action.ex
+++ repository_hub/lib/repository_hub/adapters/git/check_webhook_action.ex
@@ -1,17 +1,33 @@
 defimpl RepositoryHub.Server.CheckWebhookAction, for: RepositoryHub.GitAdapter do
   require Logger
   alias InternalApi.Repository.{CheckWebhookResponse, Webhook}
-  alias RepositoryHub.Toolkit
+
+  alias RepositoryHub.{
+    GitAdapter,
+    Toolkit,
+    OrganizationClient
+  }
+
   import Toolkit
 
   @impl true
-  def execute(_adapter, _request) do
-    %CheckWebhookResponse{
-      webhook: %Webhook{
-        url: ""
+  def execute(adapter, request) do
+    with {:ok, context} <- GitAdapter.context(adapter, request.repository_id),
+         {:ok, organization} <- get_organization(context.project.metadata.org_id) do
+      %CheckWebhookResponse{
+        webhook: %Webhook{
+          url: RepositoryHub.GitClient.Webhook.url(organization.org_username, context.repository.id)
+        }
       }
-    }
-    |> wrap()
+      |> wrap()
+    end
+  end
+
+  defp get_organization(org_id) do
+    OrganizationClient.describe(org_id)
+    |> unwrap(fn response ->
+      wrap(response.organization)
+    end)
   end
 
   @impl true
