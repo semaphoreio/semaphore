06ad3f1180b13649f51720478b010fb247173437
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-05-14 18:34:52 +0200

    Clear old filter values

--- front/assets/js/pollman_list/list.js
+++ front/assets/js/pollman_list/list.js
@@ -4,15 +4,20 @@ export class PollmanList {
   constructor() {
   }
 
+  clearAllOptions(container) {
+    const attributes = Array.from(container.attributes);
+    
+    attributes.forEach(attribute => {
+      if (attribute.name.startsWith("data-poll-param-")) {
+        container.removeAttribute(attribute.name);
+      }
+    });
+  }
+
   updateOptionsAndFetch(container, options) {
-    console.log("UPDATE")
-    console.log(options)
     Pollman.stop();
 
     for(var key in options) {
-      console.log("SET")
-      console.log("data-poll-param-" + key)
-      console.log(options[key])
       container.setAttribute("data-poll-param-" + key, options[key]);
     }
 
--- front/assets/js/workflow_list.js
+++ front/assets/js/workflow_list.js
@@ -4,46 +4,44 @@ import { TokenPagination } from "./pollman_list/token_pagination"
 import { PollmanList } from "./pollman_list/list"
 
 export var WorkflowList = {
+  initiated: false,
+  pagination: null,
+  pollmanList: null,
+  container: null,
+
   init: function() {
     if(this.initiated === true) { return; }
 
     this.initiated = true
-
     this.pagination = new TokenPagination("#workflow-lists")
-
     let pollmanList = new PollmanList;
 
     let updatePollman = function(container, params) {
-      console.log("PARAMS")
-      console.log(params)
+      const queryParams = ['page_token', 'direction', 'date_from', 'date_to'];
       pollmanList.updateOptionsAndFetch(container, params);
 
       const currentUrl = new URL(window.location.href);
-      
-      const queryParams = ['page_token', 'direction', 'date_from', 'date_to'];
       
       queryParams.forEach(queryParam => {
         currentUrl.searchParams.set(queryParam, params[queryParam] || '');
       });
       
-      console.log("current url")
-      console.log(currentUrl.toString())
-      
       window.history.pushState({}, '', currentUrl.toString());
     }
       
     this.pagination.onUpdate(updatePollman);
-    this.initFilterButtons(updatePollman);
+    this.initFilterButtons(updatePollman, pollmanList);
     this.initializeDateFilterValues();
   },
   
-  initFilterButtons: function(updatePollman) {
+  initFilterButtons: function(updatePollman, pollmanList) {
     const filterBtn = document.getElementById('filter-workflows-btn');
     const clearBtn = document.getElementById('clear-filter-btn');
+    const container = document.querySelector('.pollman-container');
     
     if (filterBtn) {
       filterBtn.addEventListener('click', () => {
-        const container = document.querySelector('.pollman-container');
+        pollmanList.clearAllOptions(container)
         const dateFrom = document.getElementById('date_from')?.value;
         const dateTo = document.getElementById('date_to')?.value;
         
@@ -63,6 +61,7 @@ export var WorkflowList = {
     
     if (clearBtn) {
       clearBtn.addEventListener('click', () => {
+        pollmanList.clearAllOptions(container)
         const dateFrom = document.getElementById('date_from');
         const dateTo = document.getElementById('date_to');
         
