b4414eb6dcbe16d033c44f8c28342a8a576ea31e
Radosław Woźniak <184065+radwo@users.noreply.github.com>
2025-05-27 13:09:52 +0200

    feat(bootstrap): do not try to recreate an organization

--- bootstrapper/pkg/organization/organization.go
+++ bootstrapper/pkg/organization/organization.go
@@ -11,9 +11,10 @@ import (
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
-func CreateSemaphoreOrganization(orgUsername, userId string) string {
+func OrganizationExists(orgUsername string) (bool, string) {
 	conn, err := grpc.NewClient(config.OrgEndpoint(), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect to org service: %v", err)
@@ -22,7 +23,43 @@ func CreateSemaphoreOrganization(orgUsername, userId string) string {
 	defer conn.Close()
 	orgClient := clients.NewOrgClient(conn)
 
-	var orgId string
+	org, err := orgClient.Describe("", orgUsername)
+	if err != nil {
+		statusErr, ok := status.FromError(err)
+		if ok && statusErr.Code() == 5 { // Not found
+			// Organization doesn't exist
+			return false, ""
+		}
+
+		// For other errors, log and assume the organization doesn't exist
+		log.Warnf("Error checking if organization exists: %v", err)
+		return false, ""
+	}
+
+	if org != nil {
+		log.Infof("Organization %s already exists with ID %s", orgUsername, org.GetOrgId())
+		return true, org.GetOrgId()
+	}
+
+	return false, ""
+}
+
+func CreateSemaphoreOrganization(orgUsername, userId string) string {
+	// First check if the organization already exists
+	exists, orgId := OrganizationExists(orgUsername)
+	if exists {
+		log.Infof("Using existing organization with username %s and ID %s", orgUsername, orgId)
+		return orgId
+	}
+
+	conn, err := grpc.NewClient(config.OrgEndpoint(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect to org service: %v", err)
+	}
+
+	defer conn.Close()
+	orgClient := clients.NewOrgClient(conn)
+
 	err = retry.WithConstantWait("organization creation", 5, 10*time.Second, func() error {
 		org, err := orgClient.Create(userId, orgUsername, orgUsername)
 		if err != nil {
