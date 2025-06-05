a704285390051438aed6e630d63d40f2d7ed36e6
Dejan K <sidejan@gmail.com>
2025-06-05 13:18:29 +0200

    feat(hooks_receiver): block hooks in ee if license is not valid

---
+++ hooks_receiver/test/license_client_test.exs
@@ -0,0 +1,35 @@
+defmodule HooksReceiver.LicenseClientTest do
+  use ExUnit.Case, async: true
+
+  alias HooksReceiver.LicenseClient
+
+  setup do
+    # Point the LicenseClient to use the mock server on localhost:50051
+    Application.put_env(:hooks_receiver, :license_checker_grpc, "localhost:50051")
+    :ok
+  end
+
+  test "returns {:ok, response} when license is valid" do
+    LicenseMock
+    |> GrpcMock.expect(:verify_license, fn _channel, _stream ->
+      %InternalApi.License.VerifyLicenseResponse{valid: true, message: "ok", expires_at: nil}
+    end)
+
+    result = LicenseClient.verify_license()
+    assert {:ok, %InternalApi.License.VerifyLicenseResponse{valid: true, message: "ok"}} = result
+    GrpcMock.verify!(LicenseMock)
+  end
+
+  test "returns {:error, reason} on error" do
+    LicenseMock
+    |> GrpcMock.expect(:verify_license, fn _channel, _stream ->
+      raise GRPC.RPCError,
+        status: :unavailable,
+        message: ""
+    end)
+
+    result = LicenseClient.verify_license()
+    assert {:error, :unavailable} = result
+    GrpcMock.verify!(LicenseMock)
+  end
+end
