987f1dc9f7c965bbbfc3a2bd0a53ebaa38a10be9
Dejan K <sidejan@gmail.com>
2025-06-06 12:16:42 +0200

    fix(hooks_receiver): add edition configuration

--- hooks_receiver/config/runtime.exs
+++ hooks_receiver/config/runtime.exs
@@ -6,5 +6,5 @@ if config_env() == :prod do
     organization_api_grpc: System.fetch_env!("INTERNAL_API_URL_ORGANIZATION"),
     license_checker_grpc:
       System.get_env("INTERNAL_API_URL_LICENSE_CHECKER", "license-checker:50051"),
-    edition: System.get_env("EDITION", "") |> String.trim() |> String.downcase()
+    edition: System.get_env("SEMAPHORE_EDITION", "") |> String.trim() |> String.downcase()
 end
