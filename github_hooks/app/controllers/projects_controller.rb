5940af1387b0905d027e4f2a8caaecf9ed2b6064
Dejan K <sidejan@gmail.com>
2025-06-06 10:15:40 +0200

    feat(github_hooks): block hooks in ee if license is not valid

--- github_hooks/app/controllers/projects_controller.rb
+++ github_hooks/app/controllers/projects_controller.rb
@@ -2,6 +2,8 @@ class ProjectsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def repo_host_post_commit_hook
+    return head :forbidden if App.ee? && !check_license!
+
     Watchman.benchmark("repo_host_post_commit_hooks.controller.duration") do
       new_request = Semaphore::RepoHost::Hooks::Request.new(repo_host_request)
 
@@ -160,5 +162,18 @@ class ProjectsController < ApplicationController
 
   def repo_host_request_params
     params
+  end
+
+  def check_license!
+    license_checker_host = App.license_checker_url
+    stub = InternalApi::License::LicenseService::Stub.new(license_checker_host, :this_channel_is_insecure)
+    begin
+      request = InternalApi::License::VerifyLicenseRequest.new
+      response = stub.verify_license(request)
+      response.valid
+    rescue StandardError => e
+      Rails.logger.error("License check error: #{e.message}")
+      false
+    end
   end
 end
