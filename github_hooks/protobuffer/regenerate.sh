c258b6bd34003e83bf203cf06f9f84ed2d1016ab
Lucas Pinheiro <lpinheiro@semaphore.io>
2025-06-12 17:31:31 -0300

    re-add user proto

--- github_hooks/protobuffer/regenerate.sh
+++ github_hooks/protobuffer/regenerate.sh
@@ -18,12 +18,11 @@ mkdir -p protobuffer/generated
 rm -rf $TMP_REPO_DIR
 git clone git@github.com:renderedtext/internal_api.git $TMP_REPO_DIR && (cd $TMP_REPO_DIR && git checkout $INTERNAL_API_BRANCH && cd -)
 
-protos=(encryptor projecthub plumber.pipeline plumber.admin google/protobuf/timestamp google/protobuf/empty google/rpc/google/protobuf/any google/rpc/code google/rpc/status repo_proxy repository_integrator internal_api/response_status server_farm.job server_farm.mq.job_state_exchange secrethub cache plumber_w_f.workflow internal_api/status repository rbac instance_config)
+protos=(encryptor projecthub user plumber.pipeline plumber.admin google/protobuf/timestamp google/protobuf/empty google/rpc/google/protobuf/any google/rpc/code google/rpc/status repo_proxy repository_integrator internal_api/response_status server_farm.job server_farm.mq.job_state_exchange secrethub cache plumber_w_f.workflow internal_api/status repository rbac instance_config)
 
 for i in ${protos[@]}; do
   echo "Generating $i"
   echo "bundle exec grpc_tools_ruby_protoc -I $TMP_REPO_DIR -I $TMP_REPO_DIR/include --ruby_out=protobuffer/generated --grpc_out=protobuffer/generated $i.proto" | sh
-  sed -E -i '' "s/require .*\/(.*_pb)'/require_relative '\1'/g" ${i}_pb.rb
 done
 
 rm -rf $TMP_REPO_DIR
