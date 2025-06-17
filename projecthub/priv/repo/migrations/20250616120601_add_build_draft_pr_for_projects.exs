097a3d0c632751c7fdb282d6f2e4aaca5c46c72b
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2025-06-17 12:57:13 +0200

    toil: linting, remove unused code

--- projecthub/priv/repo/migrations/20250616120601_add_build_draft_pr_for_projects.exs
+++ projecthub/priv/repo/migrations/20250616120601_add_build_draft_pr_for_projects.exs
@@ -2,8 +2,8 @@ defmodule Projecthub.Repo.Migrations.AddBuildDraftPrForProjects do
   use Ecto.Migration
 
   def change do
-		alter table("projects") do
-			add :build_draft_pr, :boolean, default: true, null: false
-		end
+    alter table("projects") do
+      add :build_draft_pr, :boolean, default: true, null: false
+    end
   end
 end
