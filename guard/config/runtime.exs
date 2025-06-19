42d2fb79318c04d2b44b0dd1d4f9df9993fbb302
Amir Hasanbasic <ahasanbasic@renderedtext.com>
2025-06-19 10:38:26 +0200

    fix: revert logging config to previous good setup

--- guard/config/runtime.exs
+++ guard/config/runtime.exs
@@ -2,8 +2,6 @@ import Config
 
 config :guard, :migrations_path, System.get_env("MIGRATIONS_PATH") || "/app/repo/migrations"
 
-config :logger, level: (System.get_env("LOG_LEVEL") || "debug") |> String.to_atom()
-
 {metric_channel, metrics_format} =
   System.get_env("ON_PREM")
   |> case do
