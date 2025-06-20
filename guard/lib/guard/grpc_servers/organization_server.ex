28ecfbad7783298eebcd23020a360c605b3c9313
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-06-20 14:24:44 +0200

    mix format

--- guard/lib/guard/grpc_servers/organization_server.ex
+++ guard/lib/guard/grpc_servers/organization_server.ex
@@ -488,13 +488,14 @@ defmodule Guard.GrpcServers.OrganizationServer do
           attrs =
             case proto_org.allowed_id_providers do
               [_head | _tail] ->
-              Map.put(
-                attrs,
-                :allowed_id_providers,
-                Enum.join(proto_org.allowed_id_providers, ",")
-              )
-            _ ->
-              attrs
+                Map.put(
+                  attrs,
+                  :allowed_id_providers,
+                  Enum.join(proto_org.allowed_id_providers, ",")
+                )
+
+              _ ->
+                attrs
             end
 
           case Guard.Store.Organization.update(organization, attrs) do
