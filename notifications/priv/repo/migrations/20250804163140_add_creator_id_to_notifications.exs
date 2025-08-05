a1c8ad6d036c6a521946589209ca289b9e3a955c
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-08-05 12:44:57 +0200

    Fix creator_id migration

--- notifications/priv/repo/migrations/20250804163140_add_creator_id_to_notifications.exs
+++
@@ -1,9 +0,0 @@
-defmodule Secrethub.Repo.Migrations.AddCreatorIdToNotifications do
-  use Ecto.Migration
-
-  def change do
-    alter table(:notifications) do
-      add :creator_id, :string, null: true
-    end
-  end
-end
