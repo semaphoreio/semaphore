dcfd86303bb860ef64019f1bef56737756067179
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2025-08-12 14:42:34 +0200

    fix(front): remove undefined attribute

--- front/lib/front/rbac/role_management.ex
+++ front/lib/front/rbac/role_management.ex
@@ -197,7 +197,7 @@ defmodule Front.RBAC.RoleManagement do
           requester_id: requester_id
         )
 
-      case channel() |> RBAC.RBAC.Stub.assign_role(req, timeout: @extended_grpc_timeout) do
+      case channel() |> RBAC.RBAC.Stub.assign_role(req) do
         {:ok, _resp} -> {:ok, "Role successfully assigned."}
         e -> e
       end
