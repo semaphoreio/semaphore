3d8eef63ad1a997287303c4404f7b53c6c41de64
Amir Hasanbasic <ahasanbasic@renderedtext.com>
2025-08-20 00:35:36 +0200

    fix(github_hooks): rubocop linting offenses

--- github_hooks/lib/repo_host/git/payload.rb
+++ github_hooks/lib/repo_host/git/payload.rb
@@ -15,7 +15,7 @@ module RepoHost::Git
     end
     alias is_pull_request? pull_request?
 
-    def is_draft_pull_request? # rubocop:disable Naming/PredicateName
+    def draft_pull_request?
       false
     end
 
