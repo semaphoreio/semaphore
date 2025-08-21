2d8965bf9029c12825648f427f7c713dbcb0a7ec
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2025-08-21 13:07:42 +0200

    feat(front): add finite cache ttl for running pipelines

--- front/lib/front/workflow_page/pipeline_status/model.ex
+++ front/lib/front/workflow_page/pipeline_status/model.ex
@@ -11,7 +11,8 @@ defmodule Front.WorkflowPage.PipelineStatus.Model do
   def load(pipeline_id) do
     Cacheman.fetch(:front, cache_key(pipeline_id), fn ->
       pipeline_status = load_pipeline_status_from_api(pipeline_id)
-      Cacheman.put(:front, cache_key(pipeline_id), pipeline_status)
+      cache_opts = get_cache_opts_for_status(pipeline_status)
+      Cacheman.put(:front, cache_key(pipeline_id), pipeline_status, cache_opts)
       {:ok, pipeline_status}
     end)
   end
@@ -36,4 +37,14 @@ defmodule Front.WorkflowPage.PipelineStatus.Model do
   defp pipeline_favicon_status(:DONE, :STOPPED), do: "stopped"
   defp pipeline_favicon_status(:DONE, :CANCELED), do: "canceled"
   defp pipeline_favicon_status(_, _), do: "pending"
+
+  # Running pipelines should be cached for a limited time
+  defp get_cache_opts_for_status(status) when status in ["running", "stopping", "pending"] do
+    [ttl: :timer.minutes(15)]
+  end
+
+  # Use default - infinite cache for completed pipelines
+  defp get_cache_opts_for_status(_status) do
+    []
+  end
 end
