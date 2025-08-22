4b6d92dfc944552e4e627012e369955a0f1f4ce9
Amir Hasanbasic <43892661+hamir-suspect@users.noreply.github.com>
2025-08-22 12:54:31 +0200

    feat(github_hooks): handle service account github repo host account (#519)
    ## 📝 Description
    Service accounts do not have `github_repo_host_account` hence we use
    their semaphore credentials when injecting a workflow request (either
    manual task run or manual workflow trigger via API)
    
    ## ✅ Checklist
    - [x] I have tested this change
    - [ ] This change requires documentation update

--- github_hooks/app/models/user.rb
+++ github_hooks/app/models/user.rb
@@ -6,7 +6,11 @@ class User < ActiveRecord::Base
   end
 
   def github_repo_host_account
-    repo_host_account(::Repository::GITHUB_PROVIDER)
+    account = repo_host_account(::Repository::GITHUB_PROVIDER)
+    return account if account.present?
+    return synthetic_repo_host_account if service_account?
+
+    nil
   end
 
   def bitbucket_repo_host_account
@@ -15,5 +19,39 @@ class User < ActiveRecord::Base
 
   def repo_host_account(repo_host)
     repo_host_accounts.find_by_repo_host(repo_host)
+  end
+
+  def service_account?
+    creation_source == "service_account"
+  end
+
+  private
+
+  def synthetic_repo_host_account
+    @synthetic_repo_host_account ||= SyntheticRepoHostAccount.new(self)
+  end
+
+  class SyntheticRepoHostAccount
+    attr_reader :user
+
+    def initialize(user)
+      @user = user
+    end
+
+    def name
+      user.name || "Service Account"
+    end
+
+    def github_uid
+      "service_account_#{user.id}".hash.abs.to_s
+    end
+
+    def login
+      "service-account"
+    end
+
+    def repo_host
+      ::Repository::GITHUB_PROVIDER
+    end
   end
 end
