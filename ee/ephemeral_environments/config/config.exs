cf6506d1149358119d037a08a6eb459d7939edd3
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-09-03 14:30:30 +0200

    Add explicit fetch_env! instead of get env

--- ee/ephemeral_environments/config/config.exs
+++ ee/ephemeral_environments/config/config.exs
@@ -11,9 +11,4 @@ config :ephemeral_environments, EphemeralEnvironments.Repo,
   migration_primary_key: [name: :id, type: :binary_id],
   start_apps_before_migration: [:ssl]
 
-config :watchman,
-  host: "0.0.0.0",
-  port: 8125,
-  prefix: "ephemeral_environments.#{System.get_env("METRICS_NAMESPACE") || "dev"}"
-
 import_config "#{config_env()}.exs"
--- ee/ephemeral_environments/config/runtime.exs
+++ ee/ephemeral_environments/config/runtime.exs
@@ -1,15 +1,18 @@
 import Config
 
 config :ephemeral_environments, EphemeralEnvironments.Repo,
-  database: System.get_env("POSTGRES_DB_NAME", "ephemeral_environments"),
-  username: System.get_env("POSTGRES_DB_USER", "postgres"),
-  password: System.get_env("POSTGRES_DB_PASSWORD", "the-cake-is-a-lie"),
-  hostname: System.get_env("POSTGRES_DB_HOST", "0.0.0.0"),
-  pool_size: String.to_integer(System.get_env("POSTGRES_DB_POOL_SIZE", "1")),
-  ssl: System.get_env("POSTGRES_DB_SSL", "false") == "true",
+  database: System.fetch_env!("POSTGRES_DB_NAME"),
+  username: System.fetch_env!("POSTGRES_DB_USER"),
+  password: System.fetch_env!("POSTGRES_DB_PASSWORD"),
+  hostname: System.fetch_env!("POSTGRES_DB_HOST"),
+  pool_size: String.to_integer(System.fetch_env!("POSTGRES_DB_POOL_SIZE")),
+  ssl: System.fetch_env!("POSTGRES_DB_SSL") == "true",
   ssl_opts: [verify: :verify_none]
 
-config :watchman, prefix: "ephemeral_environments.#{System.get_env("METRICS_NAMESPACE", "dev")}"
+config :watchman,
+  host: System.fetch_env!("METRICS_SIDECAR_ADDRESS"),
+  port: String.to_integer(System.fetch_env!("METRICS_SIDECAR_PORT")),
+  prefix: "ephemeral_environments.#{System.fetch_env!("METRICS_NAMESPACE")}"
 
 config :ephemeral_environments,
        :migrations_path,
