59a9d2b4116852fe4534f340a86e10b568150598
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-09-22 17:29:58 +0200

    Change DB_POOL_SIZE env var name

--- ee/ephemeral_environments/config/runtime.exs
+++ ee/ephemeral_environments/config/runtime.exs
@@ -5,13 +5,13 @@ config :ephemeral_environments, EphemeralEnvironments.Repo,
   username: System.fetch_env!("POSTGRES_DB_USER"),
   password: System.fetch_env!("POSTGRES_DB_PASSWORD"),
   hostname: System.fetch_env!("POSTGRES_DB_HOST"),
-  pool_size: String.to_integer(System.fetch_env!("POSTGRES_DB_POOL_SIZE")),
+  pool_size: String.to_integer(System.fetch_env!("DB_POOL_SIZE")),
   ssl: System.fetch_env!("POSTGRES_DB_SSL") == "true",
   ssl_opts: [verify: :verify_none]
 
 config :watchman,
-  host: System.fetch_env!("METRICS_SIDECAR_ADDRESS"),
-  port: String.to_integer(System.fetch_env!("METRICS_SIDECAR_PORT")),
+  host: System.fetch_env!("METRICS_HOST"),
+  port: String.to_integer(System.fetch_env!("METRICS_PORT")),
   prefix: "ephemeral_environments.#{System.fetch_env!("METRICS_NAMESPACE")}"
 
 config :ephemeral_environments,
