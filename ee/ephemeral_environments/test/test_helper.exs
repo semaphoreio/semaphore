66778f0c261dba5a92da97728c23381a54ca5cd8
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-09-22 14:45:11 +0200

    Export test reports

--- ee/ephemeral_environments/test/test_helper.exs
+++ ee/ephemeral_environments/test/test_helper.exs
@@ -1,2 +1,8 @@
+ExUnit.configure(
+  exclude: [integration: true],
+  capture_log: true,
+  formatters: [JUnitFormatter, ExUnit.CLIFormatter]
+)
+
 Ecto.Adapters.SQL.Sandbox.mode(EphemeralEnvironments.Repo, :manual)
 ExUnit.start()
