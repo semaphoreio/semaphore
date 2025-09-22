782f626ed452db27fe338bb5981cd1134c408cf4
VeljkoMaksimovic <maxi.maksimovic7@gmail.com>
2025-09-22 14:50:31 +0200

    Dont create test reports locally

--- ee/ephemeral_environments/test/test_helper.exs
+++ ee/ephemeral_environments/test/test_helper.exs
@@ -1,7 +1,20 @@
+formatters = [ExUnit.CLIFormatter]
+
+formatters =
+  System.get_env("CI", "")
+  |> case do
+    "" ->
+      formatters
+
+    _ ->
+      [JUnitFormatter | formatters]
+  end
+
+
 ExUnit.configure(
   exclude: [integration: true],
   capture_log: true,
-  formatters: [JUnitFormatter, ExUnit.CLIFormatter]
+  formatters: formatters
 )
 
 Ecto.Adapters.SQL.Sandbox.mode(EphemeralEnvironments.Repo, :manual)
