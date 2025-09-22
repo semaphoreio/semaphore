18fb21dda8ddffce2b51fe20b3344344a141cbb3
Veljko Maksimovic <45179708+VeljkoMaksimovic@users.noreply.github.com>
2025-09-22 12:28:56 +0200

    feat(eph): create db migrations (#552)
    ## 📝 Description
    
    https://github.com/renderedtext/project-tasks/issues/2723
    
    ## ✅ Checklist
    - [x] I have tested this change
    - [ ] This change requires documentation update

---
+++ ee/ephemeral_environments/config/test.exs
@@ -0,0 +1,11 @@
+import Config
+
+config :ephemeral_environments, EphemeralEnvironments.Repo, pool: Ecto.Adapters.SQL.Sandbox
+
+config :junit_formatter,
+  automatic_create_dir?: true,
+  report_dir: "./out",
+  report_file: "test-reports.xml",
+  print_report_file: true,
+  include_filename?: true,
+  include_file_line?: true
