05f9e1d0310f62fa6d4d3dc5f238b019d336eb1a
Veljko Maksimovic <45179708+VeljkoMaksimovic@users.noreply.github.com>
2025-09-23 12:43:41 +0200

    toil(eph): include new service into the ci (#554)
    ## 📝 Description
    
    https://github.com/renderedtext/project-tasks/issues/2740
    
    ## ✅ Checklist
    - [x] I have tested this change
    - [ ] This change requires documentation update

--- ee/ephemeral_environments/test/test_helper.exs
+++ ee/ephemeral_environments/test/test_helper.exs
@@ -1,1 +1,18 @@
+formatters =
+  System.get_env("CI", "")
+  |> case do
+    "" ->
+      [ExUnit.CLIFormatter]
+
+    _ ->
+      [JUnitFormatter, ExUnit.CLIFormatter]
+  end
+
+ExUnit.configure(
+  exclude: [integration: true],
+  capture_log: true,
+  formatters: formatters
+)
+
+Ecto.Adapters.SQL.Sandbox.mode(EphemeralEnvironments.Repo, :manual)
 ExUnit.start()
