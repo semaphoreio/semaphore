bb1a8bf3fb57a63cd21b8e5195ed7b10b0522fa6
Veljko Maksimovic <45179708+VeljkoMaksimovic@users.noreply.github.com>
2025-10-17 14:48:45 +0200

    feat(rbac): disable repo to role sync (#655)
    ## 📝 Description
    
    https://github.com/renderedtext/tasks/issues/8769
    
    ## ✅ Checklist
    - [x] I have tested this change
    - [ ] This change requires documentation update

--- ee/rbac/test/rbac/repo/repo_to_role_mapping_test.exs
+++ ee/rbac/test/rbac/repo/repo_to_role_mapping_test.exs
@@ -27,4 +27,78 @@ defmodule Rbac.Repo.RepoToRoleMapping.Test do
       assert state[:mapping_for_specific_org] === mapper
     end
   end
+
+  describe "get_project_role_from_repo_access_rights/4" do
+    test "returns nil when org has no RepoToRoleMapping for admin access" do
+      role_id =
+        RepoToRoleMapping.get_project_role_from_repo_access_rights(
+          @non_existant_org_id,
+          true,
+          false,
+          false
+        )
+
+      assert role_id === nil
+    end
+
+    test "returns admin role when user has admin access", state do
+      role_id =
+        RepoToRoleMapping.get_project_role_from_repo_access_rights(
+          @org_id,
+          true,
+          false,
+          false
+        )
+
+      assert role_id === state[:mapping_for_specific_org].admin_access_role_id
+    end
+
+    test "returns admin role when user has admin, push, and pull access", state do
+      role_id =
+        RepoToRoleMapping.get_project_role_from_repo_access_rights(
+          @org_id,
+          true,
+          true,
+          true
+        )
+
+      assert role_id === state[:mapping_for_specific_org].admin_access_role_id
+    end
+
+    test "returns push role when user has push access only", state do
+      role_id =
+        RepoToRoleMapping.get_project_role_from_repo_access_rights(
+          @org_id,
+          false,
+          true,
+          false
+        )
+
+      assert role_id === state[:mapping_for_specific_org].push_access_role_id
+    end
+
+    test "returns push role when user has push and pull access", state do
+      role_id =
+        RepoToRoleMapping.get_project_role_from_repo_access_rights(
+          @org_id,
+          false,
+          true,
+          true
+        )
+
+      assert role_id === state[:mapping_for_specific_org].push_access_role_id
+    end
+
+    test "returns pull role when user has pull access only", state do
+      role_id =
+        RepoToRoleMapping.get_project_role_from_repo_access_rights(
+          @org_id,
+          false,
+          false,
+          true
+        )
+
+      assert role_id === state[:mapping_for_specific_org].pull_access_role_id
+    end
+  end
 end
