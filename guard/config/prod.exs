1c4163b98d77d19ccff4956fd65e60cf7b9c7fe0
hamir-suspect <ahasanbasic@semaphore.io>
2025-10-27 16:48:21 +0100

    feat: github app can be created on organization and can be customized to be bulic

--- guard/config/prod.exs
+++ guard/config/prod.exs
@@ -6,4 +6,4 @@ config :oauth2, adapter: Tesla.Adapter.Hackney
 
 config :tesla, adapter: Tesla.Adapter.Hackney
 
-config :guard, :github_app_install_url, "https://github.com/settings/apps/new"
+config :guard, :github_app_base_url, "https://github.com"
