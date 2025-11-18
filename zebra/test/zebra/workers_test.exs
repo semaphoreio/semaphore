39dc0a988a9874c507896d19376c144e9db19365
Marcos Filipe <mfilipe@renderedtext.com>
2025-11-18 08:34:49 -0300

    feat(zebra): implement data deletion workers

--- zebra/test/zebra/workers_test.exs
+++ zebra/test/zebra/workers_test.exs
@@ -1,8 +1,10 @@
 defmodule Zebra.Workers.Test do
   use ExUnit.Case, async: false
 
-  test "no environment variables set => only feature provider invalidator starts" do
-    assert Zebra.Workers.active() == [Zebra.FeatureProviderInvalidatorWorker]
+  test "no environment variables set => only default workers start" do
+    assert Zebra.Workers.active() == [
+             Zebra.FeatureProviderInvalidatorWorker
+           ]
   end
 
   describe "with environment variables set" do
@@ -10,16 +12,22 @@ defmodule Zebra.Workers.Test do
       System.put_env("START_JOB_STOPPER", "true")
       System.put_env("START_TASK_FAIL_FAST_WORKER", "true")
       System.put_env("START_TASK_FINISHER_WORKER", "true")
+      System.put_env("START_JOB_DELETION_POLICY_MARKER", "true")
+      System.put_env("START_JOB_DELETION_POLICY_WORKER", "true")
 
       on_exit(fn ->
         System.put_env("START_JOB_STOPPER", "false")
         System.put_env("START_TASK_FAIL_FAST_WORKER", "false")
         System.put_env("START_TASK_FINISHER_WORKER", "false")
+        System.put_env("START_JOB_DELETION_POLICY_MARKER", "false")
+        System.put_env("START_JOB_DELETION_POLICY_WORKER", "false")
       end)
     end
 
     test "active workers are returned" do
       assert Zebra.Workers.active() == [
+               Zebra.Workers.JobDeletionPolicyMarker,
+               Zebra.Workers.JobDeletionPolicyWorker,
                Zebra.Workers.TaskFinisher,
                Zebra.Workers.TaskFailFast,
                Zebra.Workers.JobStopper,
