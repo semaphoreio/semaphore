28af12d83bc7584dc1257ba9bfb41050281483be
Marcos Filipe <mfilipe@renderedtext.com>
2025-11-18 08:34:49 -0300

    feat(zebra): implement data deletion workers

---
+++ github_hooks/db/migrate/20251119103829_add_expires_created_index_at_jobs_table.rb
@@ -0,0 +1,11 @@
+class AddExpiresCreatedIndexAtJobsTable < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
+  def change
+    add_index :jobs,
+              %i[expires_at created_at],
+              name: "index_jobs_on_expires_created_not_null",
+              algorithm: :concurrently,
+              where: "expires_at IS NOT NULL"
+  end
+end
