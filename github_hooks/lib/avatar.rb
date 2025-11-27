5f4e4ae3d6cf33604904e336379aa8f6210647e0
Dejan K <dexyk@users.noreply.github.com>
2025-11-27 16:45:16 +0100

    fix(github_hooks): create hooks payload when user doesn't have repo host account (#732)
    ## 📝 Description
    
    Allow hook payloads to be built even when a user has no attached repo
    host account by using user’s own name/email.
    
    Related [task](https://github.com/renderedtext/tasks/issues/8956).
    
    ## ✅ Checklist
    - [x] I have tested this change
    - [x] ~This change requires documentation update~ N/A

--- github_hooks/lib/avatar.rb
+++ github_hooks/lib/avatar.rb
@@ -1,5 +1,11 @@
 class Avatar
+  GENERIC_AVATAR_URL = "https://avatars2.githubusercontent.com/u/0?s=460&v=4".freeze
+
   def self.avatar_url(github_uid)
-    "https://avatars2.githubusercontent.com/u/#{github_uid}?s=460&v=4"
+    if github_uid.nil? || github_uid.to_s.start_with?("user_", "service_account_")
+      GENERIC_AVATAR_URL
+    else
+      "https://avatars2.githubusercontent.com/u/#{github_uid}?s=460&v=4"
+    end
   end
 end
