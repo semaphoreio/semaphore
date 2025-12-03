0d49ae07c9d2c4f6e1538ecb82075658c807f70d
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2025-12-03 22:53:36 +0100

    feat(plumber): add additional index for deleting

--- plumber/ppl/priv/ecto_repo/migrations/20251201141215_add_expires_at_to_pipeline_requests.exs
+++ plumber/ppl/priv/ecto_repo/migrations/20251201141215_add_expires_at_to_pipeline_requests.exs
@@ -10,5 +10,7 @@ defmodule Ppl.EctoRepo.Migrations.AddExpiresAtToPipelineRequests do
     end
 
     create index(:pipeline_requests, [:inserted_at, :expires_at], name: :idx_pipeline_requests_created_at_expires_at_not_null, concurrently: true, where: "expires_at IS NOT NULL")
+
+    create index(:pipeline_requests, [:expires_at], name: :idx_pipeline_requests_expires_at_for_deletion, concurrently: true, where: "expires_at IS NOT NULL")
   end
 end
