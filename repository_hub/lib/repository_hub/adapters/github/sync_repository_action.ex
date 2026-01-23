f753db8347a5cddb78ce544f449bca263a46d29e
Radosław Woźniak <184065+radwo@users.noreply.github.com>
2026-01-23 16:54:53 +0100

    code reformat

--- repository_hub/lib/repository_hub/adapters/github/sync_repository_action.ex
+++ repository_hub/lib/repository_hub/adapters/github/sync_repository_action.ex
@@ -32,7 +32,7 @@ defimpl RepositoryHub.SyncRepositoryAction, for: RepositoryHub.GithubAdapter do
   defp disconnect_error?(error) when is_binary(error) do
     error == "Token for not found." or
       (String.starts_with?(error, "User with id ") and
-        String.ends_with?(error, " not found"))
+         String.ends_with?(error, " not found"))
   end
 
   defp disconnect_error?(_), do: false
