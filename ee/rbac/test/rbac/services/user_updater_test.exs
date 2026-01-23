01345d7f14411438e42d8ebfb37d94d085677f95
Damjan Becirovic <damjan.becirovic@gmail.com>
2026-01-23 13:53:42 +0100

    fix(ee/rbac): Don't creat new user when loging in with GitLab account connected to existing user

--- ee/rbac/test/rbac/services/user_updater_test.exs
+++ ee/rbac/test/rbac/services/user_updater_test.exs
@@ -45,11 +45,161 @@ defmodule Rbac.Services.UserUpdaterTest do
         )
       end
     end
+
+    test "updates oidc user with gitlab identity when gitlab is connected" do
+      Rbac.Mocks.OpenIDConnect.stub_oidc_connection()
+
+      user_id = Ecto.UUID.generate()
+      oidc_user_id = Ecto.UUID.generate()
+
+      {:ok, _} = Support.Factories.RbacUser.insert(user_id, "Gitlab User", "gitlab@example.com")
+      {:ok, _} = Support.Factories.OIDCUser.insert(user_id, oidc_user_id)
+
+      {:ok, _} =
+        Support.Members.insert_repo_host_account(
+          login: "github_user",
+          github_uid: "184065",
+          user_id: user_id,
+          repo_host: "github"
+        )
+
+      {:ok, _} =
+        Support.Members.insert_repo_host_account(
+          login: "gitlab_user",
+          github_uid: "123",
+          user_id: user_id,
+          repo_host: "gitlab"
+        )
+
+      test_pid = self()
+
+      Tesla.Mock.mock_global(fn
+        %{method: :put, url: url, body: body} ->
+          if String.contains?(url, "/users/#{oidc_user_id}") do
+            send(test_pid, {:oidc_put, body})
+          end
+
+          {:ok, %Tesla.Env{status: 200, body: %{}}}
+
+        %{method: :post, url: url, body: body} ->
+          if String.contains?(url, "/users/#{oidc_user_id}/federated-identity/") do
+            send(test_pid, {:oidc_post, url, body})
+          end
+
+          {:ok, %Tesla.Env{status: 200, body: %{}}}
+
+        %{method: :delete} ->
+          {:ok, %Tesla.Env{status: 204, body: %{}}}
+      end)
+
+      with_mocks [
+        {Rbac.RoleManagement, [:passthrough],
+         assign_project_roles_to_repo_collaborators: fn _ -> :ok end,
+         assign_role: fn _, _, _ -> {:ok, nil} end},
+        {Rbac.ProviderRefresher, [:passthrough], refresh: fn _ -> :ok end},
+        {Rbac.TempSync, [:passthrough], sync_new_user_with_members_table: fn _ -> :ok end}
+      ] do
+        message =
+          %InternalApi.User.UserUpdated{user_id: user_id}
+          |> InternalApi.User.UserUpdated.encode()
+
+        Rbac.Services.UserUpdater.handle_message(message)
+      end
+
+      assert_receive {:oidc_put, body}, 5_000
+
+      body = decode_json_body(body)
+
+      assert Enum.any?(body["federatedIdentities"], fn identity ->
+               identity["identityProvider"] == "gitlab" and identity["userId"] == "123"
+             end)
+
+      assert Enum.any?(body["federatedIdentities"], fn identity ->
+               identity["identityProvider"] == "github" and identity["userId"] == "184065"
+             end)
+    end
+
+    test "updates oidc user with bitbucket identity when bitbucket is connected" do
+      Rbac.Mocks.OpenIDConnect.stub_oidc_connection()
+
+      user_id = Ecto.UUID.generate()
+      oidc_user_id = Ecto.UUID.generate()
+
+      {:ok, _} =
+        Support.Factories.RbacUser.insert(user_id, "Bitbucket User", "bitbucket@example.com")
+
+      {:ok, _} = Support.Factories.OIDCUser.insert(user_id, oidc_user_id)
+
+      {:ok, _} =
+        Support.Members.insert_repo_host_account(
+          login: "github_user",
+          github_uid: "184065",
+          user_id: user_id,
+          repo_host: "github"
+        )
+
+      {:ok, _} =
+        Support.Members.insert_repo_host_account(
+          login: "bitbucket_user",
+          github_uid: "bitbucket-uid",
+          user_id: user_id,
+          repo_host: "bitbucket"
+        )
+
+      test_pid = self()
+
+      Tesla.Mock.mock_global(fn
+        %{method: :put, url: url, body: body} ->
+          if String.contains?(url, "/users/#{oidc_user_id}") do
+            send(test_pid, {:oidc_put, body})
+          end
+
+          {:ok, %Tesla.Env{status: 200, body: %{}}}
+
+        %{method: :post, url: url, body: body} ->
+          if String.contains?(url, "/users/#{oidc_user_id}/federated-identity/") do
+            send(test_pid, {:oidc_post, url, body})
+          end
+
+          {:ok, %Tesla.Env{status: 200, body: %{}}}
+
+        %{method: :delete} ->
+          {:ok, %Tesla.Env{status: 204, body: %{}}}
+      end)
+
+      with_mocks [
+        {Rbac.Api.Bitbucket, [:passthrough],
+         user: fn "bitbucket-uid" -> {:ok, %{account_id: "bitbucket-account"}} end},
+        {Rbac.RoleManagement, [:passthrough],
+         assign_project_roles_to_repo_collaborators: fn _ -> :ok end,
+         assign_role: fn _, _, _ -> {:ok, nil} end},
+        {Rbac.ProviderRefresher, [:passthrough], refresh: fn _ -> :ok end},
+        {Rbac.TempSync, [:passthrough], sync_new_user_with_members_table: fn _ -> :ok end}
+      ] do
+        message =
+          %InternalApi.User.UserUpdated{user_id: user_id}
+          |> InternalApi.User.UserUpdated.encode()
+
+        Rbac.Services.UserUpdater.handle_message(message)
+      end
+
+      assert_receive {:oidc_put, body}, 5_000
+
+      body = decode_json_body(body)
+
+      assert Enum.any?(body["federatedIdentities"], fn identity ->
+               identity["identityProvider"] == "bitbucket" and
+                 identity["userId"] == "bitbucket-account"
+             end)
+    end
   end
 
   #
   # Helpers
   #
+
+  defp decode_json_body(body) when is_binary(body), do: Jason.decode!(body)
+  defp decode_json_body(body), do: body
 
   def publish_event(user) do
     event = %InternalApi.User.UserUpdated{user_id: user.user_id}
