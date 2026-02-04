4c9de61e1f759e020a2a02d9ab3ea1135c717a1f
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2026-02-04 10:06:38 +0100

    feat(zebra): add ubuntu2004 brownout schedule (#843)
    ## 📝 Description
    Add `ubuntu2004` brownout schedule.
    
    ## ✅ Checklist
    - [x] I have tested this change
    - [ ] This change requires documentation update

--- zebra/lib/zebra/machines/brownout.ex
+++ zebra/lib/zebra/machines/brownout.ex
@@ -3,6 +3,8 @@ defmodule Zebra.Machines.Brownout do
   This module is responsible for checking if a machine type is in brownout phase for a given organization.
   """
 
+  alias Zebra.Machines.BrownoutSchedule
+
   @type brownout_schedule :: %{
           from: DateTime.t(),
           to: DateTime.t(),
@@ -12,6 +14,22 @@ defmodule Zebra.Machines.Brownout do
   @type brownout_schedules :: [brownout_schedule]
 
   @type organization_ids :: [String.t()]
+
+  @doc """
+  Returns the combined brownout schedules for all OS images.
+  """
+  @spec schedules() :: brownout_schedules()
+  def schedules do
+    BrownoutSchedule.macosxcode14() ++ BrownoutSchedule.ubuntu2004()
+  end
+
+  @doc """
+  Checks if machine used by organization is in brownout phase using the default schedules.
+  """
+  @spec os_image_in_brownout?(Datetime.t(), String.t(), String.t()) :: boolean()
+  def os_image_in_brownout?(datetime, organization_id, machine_os_image) do
+    os_image_in_brownout?(schedules(), datetime, organization_id, machine_os_image)
+  end
 
   @doc """
   Checks if machine used by organization is in brownout phase.
