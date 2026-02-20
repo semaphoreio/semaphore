ee7db4d332646eaeff267266188fe7f8d30f070a
Radosław Woźniak <184065+radwo@users.noreply.github.com>
2026-02-20 12:59:00 +0100

    feat(github_hooks): extract GitHub app repositories to dedicated table with dual-write rollout

--- github_hooks/lib/semaphore/github_app/repositories.rb
+++ github_hooks/lib/semaphore/github_app/repositories.rb
@@ -3,9 +3,7 @@ module Semaphore::GithubApp
     MAX_NUMBER_OF_REPOSITORIES = 10000
 
     def self.refresh_by_name(organization_name)
-      installation = GithubAppInstallation.all.detect do |g|
-        (g.repositories || []).detect(&:present?).to_s.split("/")[0] == organization_name
-      end
+      installation = GithubAppInstallation.find_by_organization_name(organization_name)
 
       return :no_installation unless installation
 
@@ -25,6 +23,7 @@ module Semaphore::GithubApp
       return :low_rate_limit if client.rate_limit_remaining() < App.collaborators_api_rate_limit
 
       Semaphore::GithubApp::Hook.add_repositories(installation_id, repositories_to_add)
+      Semaphore::GithubApp::Hook.update_repository_ids(installation_id, repositories_to_update_ids)
       Semaphore::GithubApp::Hook.remove_repositories(installation_id, repositories_to_remove)
 
       :ok
@@ -33,11 +32,23 @@ module Semaphore::GithubApp
     end
 
     def repositories_to_add
-      remote_repositories - current_repositories
+      remote_repositories.select do |repository|
+        current_repositories_by_slug[canonical_slug(repository["slug"])].nil?
+      end
     end
 
     def repositories_to_remove
-      current_repositories - remote_repositories
+      current_repositories.reject do |repository|
+        remote_repositories_by_slug.key?(canonical_slug(repository["slug"]))
+      end
+    end
+
+    def repositories_to_update_ids
+      remote_repositories.select do |repository|
+        current = current_repositories_by_slug[canonical_slug(repository["slug"])]
+        current.present? &&
+          (current["id"].to_i != repository["id"].to_i || current["slug"] != repository["slug"])
+      end
     end
 
     def remote_repositories
@@ -69,15 +80,28 @@ module Semaphore::GithubApp
             "Authorization" => "token #{token}",
             "Accept" => "application/vnd.github.v3+json"
           })
-        total_count = [JSON.parse(response.data[:body])["total_count"].to_i, MAX_NUMBER_OF_REPOSITORIES].min
+        body = JSON.parse(response.data[:body])
+        total_count = [body["total_count"].to_i, MAX_NUMBER_OF_REPOSITORIES].min
 
-        github_repos = github_repos + JSON.parse(response.data[:body])["repositories"].map { |repo| repo["full_name"] }
-        break if page * per_page > total_count
+        github_repos.concat(Semaphore::GithubApp::Hook.map_repositories(body["repositories"]))
+        break if page * per_page >= total_count
         page += 1
         sleep 1
       end
 
       github_repos
+    end
+
+    def current_repositories_by_slug
+      @current_repositories_by_slug ||= current_repositories.index_by { |repository| canonical_slug(repository["slug"]) }
+    end
+
+    def remote_repositories_by_slug
+      @remote_repositories_by_slug ||= remote_repositories.index_by { |repository| canonical_slug(repository["slug"]) }
+    end
+
+    def canonical_slug(slug)
+      GithubAppInstallation.canonical_slug(slug)
     end
 
     def token
--- github_hooks/spec/factories/github_app_installations.rb
+++ github_hooks/spec/factories/github_app_installations.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :github_app_installation do
     installation_id { 13609976 }
-    repositories { ["renderedtext/guard", "semaphoreio/semaphore"] }
+    repositories do
+      [
+        { "id" => 137368312, "slug" => "renderedtext/guard" },
+        { "id" => 217099396, "slug" => "semaphoreio/semaphore" }
+      ]
+    end
   end
 end
