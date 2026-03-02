a3fbb71861449bf4485d101c08e5d4d83cdef4ed
Corey Christous <cchristous@confluent.io>
2026-03-01 21:25:25 -0500

    fix: replace magic number with named constant and add UNAVAILABLE test coverage
    Use @grpc_unavailable module attribute instead of hardcoded 14 in
    hooks_processor for consistency with schedule_wf_impl and run_now_impl.
    Add UNAVAILABLE tests for all 5 untested v1alpha response formatters.

--- hooks_processor/lib/hooks_processor/hooks/grpc/server.ex
+++ hooks_processor/lib/hooks_processor/hooks/grpc/server.ex
@@ -11,6 +11,8 @@ defmodule HooksProcessor.Hooks.Grpc.Server do
   alias HooksProcessor.Clients.{UserClient, ProjectHubClient, BranchClient, WorkflowClient, RepositoryClient}
   alias InternalApi.RepoProxy.{CreateResponse, CreateBlankResponse}
 
+  @grpc_unavailable GRPC.Status.unavailable()
+
   def describe(_, _) do
     raise GRPC.RPCError, status: GRPC.Status.unimplemented(), message: "Not yet implemented!"
   end
@@ -42,7 +44,7 @@ defmodule HooksProcessor.Hooks.Grpc.Server do
            {:ok, hook} <- process_hook(rpc, hook_type, hook, project.repository, user.id) do
         form_response(rpc, hook)
       else
-        {:error, %GRPC.RPCError{status: 14} = rpc_error} ->
+        {:error, %GRPC.RPCError{status: @grpc_unavailable} = rpc_error} ->
           raise rpc_error
 
         error ->
