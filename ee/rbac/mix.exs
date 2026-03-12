d031c65765d1264fc43ce49b11e54d039ff3fe73
Damjan Bećirović <damjan.becirovic@gmail.com>
2026-03-12 16:45:40 +0100

    Change esaml dependency to be fetched from original handnot2/esaml repository  (#925)
    The dropbox/esaml GitHub repository is no longer available, so the build
    process is failing.
    This PR switches the dependency to be fetched from handnot2/esaml
    repository which is the original one from which the dropbox/esaml was
    forked.
    We are using the older version that is also available on the original
    repository.

--- ee/rbac/mix.exs
+++ ee/rbac/mix.exs
@@ -39,7 +39,7 @@ defmodule Rbac.MixProject do
       {:yaml_elixir, "~> 2.11"},
       {:bypass, "~> 2.1", [only: :test]},
       {:jose, "~> 1.11"},
-      {:esaml, git: "https://github.com/dropbox/esaml", tag: "v4.2.0"},
+      {:esaml, git: "https://github.com/handnot2/esaml", tag: "v4.2.0"},
       {:plug_cowboy, "~> 2.0"},
       {:cowboy, "~> 2.12.0", override: true},
       {:cowlib, "~> 2.13.0", override: true},
