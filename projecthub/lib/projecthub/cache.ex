fe3f5bf4f0e052848a0e3eea26e8fbce66d13ab0
Damjan Becirovic <damjan.becirovic@gmail.com>
2026-03-13 11:49:45 +0100

    feat(projecthub): send ceph-aware cache create payload and add provision rpc client
    (cherry picked from commit 9ccedc1b5f95c687cae90498f8e5d6f1800e41bd)

--- projecthub/lib/projecthub/cache.ex
+++ projecthub/lib/projecthub/cache.ex
@@ -1,33 +1,31 @@
 defmodule Projecthub.Cache do
   require Logger
   alias Projecthub.Models.Project
+  alias InternalApi.Cache.CacheService.Stub
 
   def create_for_project(project_id, metadata \\ nil) do
     Logger.info("Creating cache for project: #{project_id}")
 
-    req = InternalApi.Cache.CreateRequest.new()
+    with {:ok, project} <- Project.find(project_id),
+         req <- build_create_request(project),
+         {:ok, res} <- Stub.create(channel(), req, options(metadata)) do
+      if res.status.code == :OK do
+        {:ok, _} = update_project(project_id, res.cache_id)
 
-    {:ok, res} =
-      InternalApi.Cache.CacheService.Stub.create(
-        channel(),
-        req,
-        options(metadata)
-      )
+        Logger.info("Successfully created cache for project: #{project_id}")
+      else
+        Logger.error("Couldn't create cache for project: #{project_id}: #{inspect(res)}")
 
-    if res.status.code == :OK do
-      {:ok, _} = update_project(project_id, res.cache_id)
-
-      Logger.info("Successfully created cache for project: #{project_id}")
+        nil
+      end
     else
-      Logger.error("Couldn't create cache for project: #{project_id}: #{inspect(res)}")
-
-      nil
+      {:error, reason} ->
+        Logger.error("Couldn't create cache for project: #{project_id}: #{inspect(reason)}")
+        nil
     end
   end
 
   def destroy(cache_id, project_id, metadata \\ nil) do
-    alias InternalApi.Cache.CacheService.Stub
-
     with req <- InternalApi.Cache.DestroyRequest.new(cache_id: cache_id),
          {:ok, _} <- Stub.destroy(channel(), req, options(metadata)) do
       Watchman.increment("cachehub.destroy.succeeded")
@@ -43,9 +41,59 @@ defmodule Projecthub.Cache do
     end
   end
 
+  def provision_ceph_cache(cache_id, organization_id, project_id, project_name \\ nil, metadata \\ nil) do
+    req =
+      InternalApi.Cache.ProvisionCephCacheRequest.new(
+        cache_id: cache_id,
+        organization_id: organization_id,
+        project_id: project_id,
+        project_name: project_name || ""
+      )
+
+    case Stub.provision_ceph_cache(channel(), req, options(metadata)) do
+      {:ok, res} when res.status.code == :OK ->
+        Logger.info(
+          "Queued Ceph cache provisioning cache_id=#{cache_id} org_id=#{organization_id} project_id=#{project_id}"
+        )
+
+        {:ok, res}
+
+      {:ok, res} ->
+        Logger.error(
+          "Failed to queue Ceph cache provisioning cache_id=#{cache_id} org_id=#{organization_id} project_id=#{project_id} status=#{inspect(res.status)}"
+        )
+
+        {:error, res}
+
+      {:error, reason} ->
+        Logger.error(
+          "Ceph cache provisioning call failed cache_id=#{cache_id} org_id=#{organization_id} project_id=#{project_id} reason=#{inspect(reason)}"
+        )
+
+        {:error, reason}
+    end
+  end
+
   defp update_project(project_id, cache_id) do
     {:ok, project} = Project.find(project_id)
     Project.update_record(project, %{cache_id: cache_id})
+  end
+
+  defp build_create_request(project) do
+    InternalApi.Cache.CreateRequest.new(
+      organization_id: project.organization_id,
+      project_id: project.id,
+      project_name: project.name,
+      backend: backend_for_org(project.organization_id)
+    )
+  end
+
+  defp backend_for_org(org_id) do
+    if FeatureProvider.feature_enabled?(:use_ceph_for_cache, param: org_id) do
+      InternalApi.Cache.Backend.value(:CEPH)
+    else
+      InternalApi.Cache.Backend.value(:SFTP)
+    end
   end
 
   defp channel do
