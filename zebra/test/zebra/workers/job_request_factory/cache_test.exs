0963377acc44e561c9404c2c92269e4b2dd6c51a
Damjan Becirovic <damjan.becirovic@gmail.com>
2026-03-18 11:19:26 +0100

    test(zebra): add test to cover ceph sts client and changes to cache env vars injection

--- zebra/test/zebra/workers/job_request_factory/cache_test.exs
+++ zebra/test/zebra/workers/job_request_factory/cache_test.exs
@@ -4,20 +4,76 @@ defmodule Zebra.Workers.JobRequestFactory.CacheTest do
 
   alias Zebra.Workers.JobRequestFactory.Cache
 
+  defmodule CephStsSuccessMock do
+    def assume_role(role_arn, session_name, duration_seconds) do
+      send(self(), {:assume_role_called, role_arn, session_name, duration_seconds})
+
+      {:ok,
+       %{
+         access_key_id: "tmp-access",
+         secret_access_key: "tmp-secret",
+         session_token: "tmp-token"
+       }}
+    end
+  end
+
+  defmodule CephStsFailureMock do
+    def assume_role(_role_arn, _session_name, _duration_seconds), do: {:error, :boom}
+  end
+
   @org_id Ecto.UUID.generate()
   @cache_id Ecto.UUID.generate()
-  @cache InternalApi.Cache.Cache.new(
-           id: @cache_id,
-           credential: "--BEGIN....lalalala...cache_key...END---",
-           url: "localhost:29920"
-         )
 
-  describe ".env_vars" do
+  @sftp_cache InternalApi.Cache.Cache.new(
+                id: @cache_id,
+                credential: "--BEGIN....lalalala...cache_key...END---",
+                url: "localhost:29920",
+                backend: InternalApi.Cache.Backend.value(:SFTP)
+              )
+
+  @ceph_cache InternalApi.Cache.Cache.new(
+                id: @cache_id,
+                backend: InternalApi.Cache.Backend.value(:CEPH),
+                state: InternalApi.Cache.CacheState.value(:READY),
+                bucket: "project-bucket",
+                ro_role_arn: "arn:aws:iam::acc:role/project-ro",
+                rw_role_arn: "arn:aws:iam::acc:role/project-rw"
+              )
+
+  setup do
+    Application.put_env(:zebra, :ceph_sts_client_module, CephStsSuccessMock)
+    System.put_env("CEPH_ENDPOINT", "https://ceph.example.com")
+
+    on_exit(fn ->
+      Application.delete_env(:zebra, :ceph_sts_client_module)
+      System.delete_env("CEPH_ENDPOINT")
+    end)
+
+    :ok
+  end
+
+  describe ".files" do
+    test "sftp cache => injects private key file" do
+      {:ok, job} = Support.Factories.Job.create(:pending, %{})
+      {:ok, files} = Cache.files(job, @sftp_cache)
+
+      assert files == [
+               %{
+                 "path" => "/home/semaphore/.ssh/semaphore_cache_key",
+                 "content" => Base.encode64("--BEGIN....lalalala...cache_key...END---"),
+                 "mode" => "0600"
+               }
+             ]
+    end
+
+    test "ceph cache => does not inject key file" do
+      {:ok, job} = Support.Factories.Job.create(:pending, %{})
+      assert {:ok, []} = Cache.files(job, @ceph_cache)
+    end
+  end
+
+  describe ".env_vars sftp" do
     test "cache_cli_parallel_archive_method is enabled => uses parallel archive method" do
-      #
-      # stubbed feature provider has feature disabled,
-      # so we need to enable it here.
-      #
       Mox.stub(Support.MockedProvider, :provide_features, fn _, _ ->
         features =
           Support.StubbedProvider.provide_features()
@@ -37,20 +93,80 @@ defmodule Zebra.Workers.JobRequestFactory.CacheTest do
       end)
 
       {:ok, job} = Support.Factories.Job.create(:pending, %{})
-      {:ok, envs} = Cache.env_vars(job, @cache, @org_id)
-      expected_envs = expected_envs(true)
+      {:ok, envs} = Cache.env_vars(job, @sftp_cache, @org_id, nil, :pipeline_job)
+      expected_envs = expected_sftp_envs(true)
       assert envs == expected_envs
     end
 
     test "cache_cli_parallel_archive_method is disabled => does not use parallel archive method" do
       {:ok, job} = Support.Factories.Job.create(:pending, %{})
-      {:ok, envs} = Cache.env_vars(job, @cache, @org_id)
-      expected_envs = expected_envs(false)
+      {:ok, envs} = Cache.env_vars(job, @sftp_cache, @org_id, nil, :pipeline_job)
+      expected_envs = expected_sftp_envs(false)
       assert envs == expected_envs
     end
   end
 
-  defp expected_envs(new_method_enabled) do
+  describe ".env_vars ceph" do
+    test "non-forked job => uses RW role and injects temporary AWS credentials" do
+      {:ok, job} = Support.Factories.Job.create(:pending, %{})
+
+      repo_proxy = %{repo_slug: "org/repo", pr_slug: ""}
+      {:ok, envs} = Cache.env_vars(job, @ceph_cache, @org_id, repo_proxy, :pipeline_job)
+
+      assert_receive {:assume_role_called, "arn:aws:iam::acc:role/project-rw", _session, 87_300}
+
+      assert envs == [
+               %{"name" => "SEMAPHORE_CACHE_BACKEND", "value" => Base.encode64("s3")},
+               %{
+                 "name" => "SEMAPHORE_CACHE_S3_URL",
+                 "value" => Base.encode64("https://ceph.example.com")
+               },
+               %{
+                 "name" => "SEMAPHORE_CACHE_S3_BUCKET",
+                 "value" => Base.encode64("project-bucket")
+               },
+               %{"name" => "AWS_ACCESS_KEY_ID", "value" => Base.encode64("tmp-access")},
+               %{"name" => "AWS_SECRET_ACCESS_KEY", "value" => Base.encode64("tmp-secret")},
+               %{"name" => "AWS_SESSION_TOKEN", "value" => Base.encode64("tmp-token")}
+             ]
+    end
+
+    test "forked PR regular job => uses RO role and regular STS duration" do
+      {:ok, job} = Support.Factories.Job.create(:pending, %{})
+
+      repo_proxy = %{repo_slug: "base/repo", pr_slug: "fork/repo"}
+      {:ok, _envs} = Cache.env_vars(job, @ceph_cache, @org_id, repo_proxy, :pipeline_job)
+
+      assert_receive {:assume_role_called, "arn:aws:iam::acc:role/project-ro", _session, 87_300}
+    end
+
+    test "forked PR debug job => uses RO role and debug STS duration" do
+      {:ok, job} = Support.Factories.Job.create(:pending, %{})
+
+      repo_proxy = %{repo_slug: "base/repo", pr_slug: "fork/repo"}
+      {:ok, _envs} = Cache.env_vars(job, @ceph_cache, @org_id, repo_proxy, :debug_job)
+
+      assert_receive {:assume_role_called, "arn:aws:iam::acc:role/project-ro", _session, 4_200}
+    end
+
+    test "sts failure => cache env vars are skipped" do
+      Application.put_env(:zebra, :ceph_sts_client_module, CephStsFailureMock)
+
+      {:ok, job} = Support.Factories.Job.create(:pending, %{})
+      repo_proxy = %{repo_slug: "org/repo", pr_slug: ""}
+
+      assert {:ok, []} = Cache.env_vars(job, @ceph_cache, @org_id, repo_proxy, :pipeline_job)
+    end
+  end
+
+  test "session_name_for_job normalizes disallowed chars and limits length" do
+    session_name = Cache.session_name_for_job("ABC/123:weird*chars-#{String.duplicate("x", 80)}", true)
+
+    assert byte_size(session_name) <= 64
+    assert session_name =~ ~r/^[a-z0-9+=,.@-]+$/
+  end
+
+  defp expected_sftp_envs(new_method_enabled) do
     vars = [
       %{
         "name" => "SSH_PRIVATE_KEY_PATH",
---
+++ zebra/test/zebra/workers/job_request_factory/ceph_sts_test.exs
@@ -0,0 +1,93 @@
+defmodule Zebra.Workers.JobRequestFactory.CephStsTest do
+  use ExUnit.Case, async: false
+
+  alias Zebra.Workers.JobRequestFactory.CephSts
+
+  defmodule SignerMock do
+    def sign_v4(_ak, _sk, _region, _service, _time, _method, _url, headers, _body) do
+      send(self(), {:sign_called, headers})
+      [{"authorization", "AWS4-HMAC-SHA256 test"} | headers]
+    end
+  end
+
+  defmodule HttpClientSuccessMock do
+    def request(:post, _request, _opts, _http_opts) do
+      body =
+        """
+        <AssumeRoleResponse>
+          <AssumeRoleResult>
+            <Credentials>
+              <AccessKeyId>TMP_ACCESS</AccessKeyId>
+              <SecretAccessKey>TMP_SECRET</SecretAccessKey>
+              <SessionToken>TMP_TOKEN</SessionToken>
+            </Credentials>
+          </AssumeRoleResult>
+        </AssumeRoleResponse>
+        """
+
+      {:ok, {{'HTTP/1.1', 200, 'OK'}, [], body}}
+    end
+  end
+
+  defmodule HttpClientErrorMock do
+    def request(:post, _request, _opts, _http_opts) do
+      body =
+        """
+        <ErrorResponse>
+          <Error>
+            <Code>AccessDenied</Code>
+            <Message>Denied</Message>
+          </Error>
+        </ErrorResponse>
+        """
+
+      {:ok, {{'HTTP/1.1', 403, 'Forbidden'}, [], body}}
+    end
+  end
+
+  setup do
+    System.put_env("CEPH_ENDPOINT", "https://ceph.example.com")
+    System.put_env("CEPH_ZEBRA_ACCESS_KEY", "zebra-ak")
+    System.put_env("CEPH_ZEBRA_SECRET_KEY", "zebra-sk")
+
+    Application.put_env(:zebra, :ceph_sts_signer_module, SignerMock)
+
+    on_exit(fn ->
+      System.delete_env("CEPH_ENDPOINT")
+      System.delete_env("CEPH_ZEBRA_ACCESS_KEY")
+      System.delete_env("CEPH_ZEBRA_SECRET_KEY")
+      Application.delete_env(:zebra, :ceph_sts_signer_module)
+      Application.delete_env(:zebra, :ceph_sts_http_client_module)
+    end)
+
+    :ok
+  end
+
+  test "assume_role returns temporary credentials on success" do
+    Application.put_env(:zebra, :ceph_sts_http_client_module, HttpClientSuccessMock)
+
+    assert {:ok,
+            %{access_key_id: "TMP_ACCESS", secret_access_key: "TMP_SECRET", session_token: "TMP_TOKEN"}} =
+             CephSts.assume_role("arn:aws:iam::acc:role/project-rw", "zebra-rw-job", 3600)
+
+    assert_receive {:sign_called, headers}
+    assert Enum.any?(headers, fn {name, _} -> name == "host" end)
+  end
+
+  test "assume_role returns sts_error when STS returns XML error" do
+    Application.put_env(:zebra, :ceph_sts_http_client_module, HttpClientErrorMock)
+
+    assert {:error, {:sts_error, "AccessDenied", "Denied"}} =
+             CephSts.assume_role("arn:aws:iam::acc:role/project-rw", "zebra-rw-job", 3600)
+  end
+
+  test "assume_role returns missing_config when required env vars are absent" do
+    System.delete_env("CEPH_ZEBRA_SECRET_KEY")
+    Application.put_env(:zebra, :ceph_sts_http_client_module, HttpClientSuccessMock)
+
+    assert {:error, {:missing_config, missing}} =
+             CephSts.assume_role("arn:aws:iam::acc:role/project-rw", "zebra-rw-job", 3600)
+
+    assert "CEPH_ZEBRA_SECRET_KEY" in missing
+  end
+end
