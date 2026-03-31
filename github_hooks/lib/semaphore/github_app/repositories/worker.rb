f53b8055f0977f5bd4767faec482e472c2fc3946
Radosław Woźniak <184065+radwo@users.noreply.github.com>
2026-03-31 18:28:25 +0200

    fix linting

--- github_hooks/lib/semaphore/github_app/repositories/worker.rb
+++ github_hooks/lib/semaphore/github_app/repositories/worker.rb
@@ -46,8 +46,8 @@ module Semaphore::GithubApp
           log(installation_id, "Unknown result: #{result.inspect}")
           delete_unique_lock([installation_id])
         end
-      rescue Semaphore::GithubApp::Repositories::IncompleteRepositoryListError => exception
-        log(installation_id, "Incomplete repository list — raising to trigger retry with backoff: #{exception.message}")
+      rescue Semaphore::GithubApp::Repositories::IncompleteRepositoryListError => e
+        log(installation_id, "Incomplete repository list — raising to trigger retry with backoff: #{e.message}")
         raise
       end
 
