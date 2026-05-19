5f70909ab08f3a147e012a543d70433934a93855
Marcos Filipe <mfilipe@renderedtext.com>
2026-05-19 10:47:11 -0300

    fix(v1alpha): extract shared authorize to fix Credo duplication
    is_authorized? duplicated across 4 authorize modules (mass 44x3).
    Extract to SharedAuthorize.check_permission/2, delegates only.

--- public-api/v1alpha/lib/pipelines_api/roles/authorize.ex
+++ public-api/v1alpha/lib/pipelines_api/roles/authorize.ex
@@ -2,45 +2,9 @@ defmodule PipelinesAPI.Roles.Authorize do
   @moduledoc false
   use Plug.Builder
 
-  alias PipelinesAPI.RBACClient
-  alias LogTee, as: LT
-  alias Plug.Conn
+  def authorize_view_roles(conn, _opts),
+    do: PipelinesAPI.SharedAuthorize.check_permission("organization.custom_roles.view", conn)
 
-  def authorize_view_roles(conn, _opts) do
-    is_authorized?("organization.custom_roles.view", conn)
-  end
-
-  def authorize_manage_roles(conn, _opts) do
-    is_authorized?("organization.custom_roles.manage", conn)
-  end
-
-  defp is_authorized?(permission, conn) do
-    user_id = Conn.get_req_header(conn, "x-semaphore-user-id") |> Enum.at(0, "")
-    org_id = Conn.get_req_header(conn, "x-semaphore-org-id") |> Enum.at(0, "")
-
-    if user_id == "" or org_id == "" do
-      conn |> resp(404, "Not found") |> halt
-    else
-      is_authorized_check(permission, conn, user_id, org_id)
-    end
-  end
-
-  defp is_authorized_check(permission, conn, user_id, org_id) do
-    with params <- %{user_id: user_id, org_id: org_id},
-         {:ok, permissions} <- RBACClient.list_user_permissions(params) do
-      if Enum.member?(permissions, permission) do
-        conn
-      else
-        conn |> resp(403, "Permission denied") |> halt
-      end
-    else
-      {:error, {:internal, _}} = error ->
-        LT.error(error, "Roles.Authorize")
-        conn |> resp(500, "Internal error") |> halt
-
-      error ->
-        LT.error(error, "Roles.Authorize")
-        conn |> resp(404, "Not found") |> halt
-    end
-  end
+  def authorize_manage_roles(conn, _opts),
+    do: PipelinesAPI.SharedAuthorize.check_permission("organization.custom_roles.manage", conn)
 end
