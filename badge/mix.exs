f9252431d34bd58e8ac95e1d9d4b9ee423ff7dd1
Dejan K <sidejan@gmail.com>
2026-06-01 16:53:24 +0200

    deps(badge): update dependencies to fix CVEs

--- badge/mix.exs
+++ badge/mix.exs
@@ -27,13 +27,17 @@ defmodule Badges.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug, "~> 1.8.3"},
-      {:plug_cowboy, "~> 2.0"},
-      {:grpc, "~> 0.3.1"},
+      {:plug, "~> 1.15.4"},
+      {:plug_cowboy, "~> 2.8.1"},
+      {:cowboy, "~> 2.15.0", override: true},
+      {:cowlib, "~> 2.16.1", override: true},
+      {:grpc, "0.5.0-beta.1", override: true},
+      {:protobuf, "~> 0.6.3", override: true},
       {:grpc_mock, github: "renderedtext/grpc-mock", only: [:dev, :test]},
       {:watchman, github: "renderedtext/ex-watchman"},
       {:cachex, "~> 3.2"},
       {:sentry, "~> 7.0"},
+      {:hackney, "~> 1.24", override: true},
       {:jason, "~> 1.1"},
       {:mix_audit, "~> 0.1", only: [:dev, :test], runtime: false},
       {:sobelow, "~> 0.8", only: :dev},
