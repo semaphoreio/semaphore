f6bce1c577862c04aaaa473ff2a350f77939c9b3
Mikołaj Kutryj <mikolaj.kutryj@gmail.com>
2026-06-03 15:10:20 +0200

    feat(public-api): GET test_results/flaky_tests/:test_id endpoint

---
+++ public-api/v1alpha/lib/pipelines_api/test_results/flaky_test_details.ex
@@ -0,0 +1,28 @@
+defmodule PipelinesAPI.TestResults.FlakyTestDetails do
+  @moduledoc "GET /projects/:project_id/test_results/flaky_tests/:test_id"
+
+  use Plug.Builder
+
+  alias PipelinesAPI.SuperjerryClient
+  alias PipelinesAPI.Pipelines.Common, as: RespCommon
+  alias PipelinesAPI.Util.Metrics
+
+  import PipelinesAPI.TestResults.Common, only: [track_request_metrics: 2, feature_enabled: 2]
+  import PipelinesAPI.TestResults.Authorize, only: [authorize_read: 2]
+
+  plug(:track_request_metrics)
+  plug(:feature_enabled)
+  plug(:authorize_read)
+  plug(:details)
+
+  def details(conn, _opts) do
+    Metrics.benchmark("PipelinesAPI.router", ["test_results_flaky_test_details"], fn ->
+      org_id = Plug.Conn.get_req_header(conn, "x-semaphore-org-id") |> Enum.at(0, "")
+
+      conn.params
+      |> Map.put("org_id", org_id)
+      |> SuperjerryClient.flaky_test_details()
+      |> RespCommon.respond(conn)
+    end)
+  end
+end
