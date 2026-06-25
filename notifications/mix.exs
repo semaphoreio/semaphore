c14da91d2a8de9bc9d5f1694a95665a04e5c5441
Marcos Filipe <mfilipe@renderedtext.com>
2026-06-25 08:46:57 +0000

    fix: address CI failures

--- notifications/mix.exs
+++ notifications/mix.exs
@@ -40,7 +40,7 @@ defmodule Notifications.Mixfile do
       {:mock, "~> 0.3.0", only: [:dev, :test]},
       {:paginator, "~> 1.0"},
       {:tackle, github: "renderedtext/ex-tackle"},
-      {:util, github: "renderedtext/elixir-util", branch: "rw/string_enums"},
+      {:util, github: "renderedtext/elixir-util", ref: "d45b317c405e38229187e07e19532e726bd7c095"},
       {:sentry, "~> 8.0"},
       {:hackney, "~> 1.8"},
       {:sentry_grpc, github: "renderedtext/sentry_grpc"},
