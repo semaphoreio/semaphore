def9b65aea1e57a6bc40216016e98348aceb2384
Dejan K <sidejan@gmail.com>
2026-09-08 11:52:26 +0200

    feat(repohub): add benchmarking functionality with logging for repository operations

--- repohub/pkg/gitrekt/commit.go
+++ repohub/pkg/gitrekt/commit.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	git "github.com/libgit2/git2go/v34"
-	"github.com/renderedtext/go-watchman"
+	bench "github.com/semaphoreio/semaphore/repohub/pkg/bench"
 )
 
 func check(err error, message string) {
@@ -38,7 +38,7 @@ type CommitPayload struct {
 }
 
 func Commit(repo *Repository, payload CommitPayload) (rev *Revision, err error) {
-	defer watchman.Benchmark(time.Now(), "gitrekt.commit")
+	defer bench.Observe(time.Now(), "gitrekt.commit", repo.HttpURL)
 
 	defer func() {
 		if r := recover(); r != nil {
